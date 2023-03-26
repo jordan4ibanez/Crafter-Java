a0669d79e10d8cee2e73dc291f32b805d36e2705
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-25 22:21:09 -0400

    Create some basic framework

---
+++ src/main/java/org/crafter/Engine/Window.java
@@ -0,0 +1,22 @@
+package org.crafter.Engine;
+
+/**
+ * Window interops with OpenGL 4.1 and GLFW 3.3.
+ * There can be only 1 Window in this game. So it's a final class with static methods.
+ */
+public final class Window {
+
+    private Window() {}
+
+    public static void initialize() {
+        System.out.println("Well hello dere");
+        System.out.println("I am a fancy java boi");
+        System.out.printf("Now I will break the terminal");
+    }
+
+    public static void destroy() {
+
+    }
+
+
+}
--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -1,7 +1,16 @@
 package org.crafter;
 
+import org.crafter.Engine.Window;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Window.initialize();
+
+
+
+        Window.destroy();
+
     }
 }
