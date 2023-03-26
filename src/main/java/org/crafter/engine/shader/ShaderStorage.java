7d007d825ffb2d0235ecf07ed30056634eae4d03
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-26 04:05:49 -0400

    Create a set hashmap

--- src/main/java/org/crafter/engine/shader/ShaderStorage.java
+++ src/main/java/org/crafter/engine/shader/ShaderStorage.java
@@ -7,4 +7,12 @@ public final class ShaderStorage {
     private static final HashMap<String, Shader> container = new HashMap<String, Shader>();
 
     private ShaderStorage(){}
+
+    // Create a new shader for the program
+    public static void createShader(String shaderName, String vertexCodeLocation, String fragmentCodeLocation) {
+        if (container.containsKey(shaderName)) {
+            throw new RuntimeException("ShaderStorage: Tried to add " + shaderName + " more than once!");
+        }
+        container.put(shaderName, new Shader(vertexCodeLocation, fragmentCodeLocation));
+    }
 }
