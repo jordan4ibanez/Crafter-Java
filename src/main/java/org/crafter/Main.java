f397b5eb6fd60c2719d462b64b8cab97428edcb6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-26 03:53:08 -0400

    Update

--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -1,5 +1,7 @@
 package org.crafter;
 
+import org.crafter.engine.Window;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/main/java/org/crafter/engine/shader/Shader.java
@@ -0,0 +1,101 @@
+package org.crafter.engine.shader;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import static org.lwjgl.opengl.GL20.*;
+
+/**
+ * This is an OpenGL Shader Program!
+ * Written as concisely as possible. :)
+ */
+public class Shader {
+
+    private final int programID;
+
+    // You have to have your code available at construction!
+    Shader (String vertexLocation, String fragmentLocation) {
+
+        programID = glCreateProgram();
+
+        if (programID == 0) {
+            throw new RuntimeException("Shader: Failed to create shader!");
+        }
+
+        final int vertexID = compileCode(vertexLocation, GL_VERTEX_SHADER);
+
+        final int fragmentID = compileCode(fragmentLocation, GL_FRAGMENT_SHADER);
+
+        link(vertexID, fragmentID);
+    }
+
+    // Now bake the whole pipeline into the program
+    private void link(int vertexID, int fragmentID) {
+        glLinkProgram(programID);
+
+        if (glGetProgrami(programID, GL_LINK_STATUS) == 0) {
+            //1024 max chars
+            throw new RuntimeException("Shader: Error linking shader code! " + glGetProgramInfoLog(programID, 1024));
+        }
+
+        // Why would the ID ever be 0? If it's 0 something went horribly wrong
+        if (vertexID != 0) {
+            glDetachShader(programID, vertexID);
+        }
+        if (fragmentID != 0) {
+            glDetachShader(programID, fragmentID);
+        }
+
+        glValidateProgram(programID);
+
+        // Note: This should probably just crash the program if there's a validation error
+        if (glGetProgrami(programID, GL_VALIDATE_STATUS) == 0) {
+            System.out.println("Shader: VALIDATION WARNING! " + glGetProgramInfoLog(programID, 1024));
+        }
+    }
+
+    // Turn that fancy text into machine code
+     private int compileCode(String fileLocation, int shaderType) {
+
+        String code = getFileString(fileLocation);
+
+        int shaderID = glCreateShader(shaderType);
+
+        if (shaderID == 0) {
+            String shaderTypeString = shaderType == GL_VERTEX_SHADER ? "GL_VERTEX_SHADER" : "GL_FRAGMENT_SHADER";
+            throw new RuntimeException("Shader: Failed to create shader " + shaderTypeString + " located at " + fileLocation + "!");
+        }
+
+        glShaderSource(shaderID, code);
+
+        glCompileShader(shaderID);
+
+        if (glGetShaderi(shaderID, GL_COMPILE_STATUS) == 0) {
+            //1024 max chars
+            throw new RuntimeException("Shader: Error compiling shader code! " + glGetShaderInfoLog(shaderID, 1024));
+        }
+
+        glAttachShader(programID, shaderID);
+
+        return shaderID;
+    }
+
+    // Internal auto checker to load file
+    private String getFileString(String fileLocation) {
+        // Check if it exists
+        File file = new File(fileLocation);
+        if (!file.exists()) {
+            throw new RuntimeException("Shader: File " + fileLocation + " does not exist!");
+        }
+
+        // Now try to read it
+        String code;
+        try {
+            code = Files.readString(file.toPath());
+        } catch (Exception errorString) {
+            throw new RuntimeException("Shader: Failed to read file " + fileLocation + "! Error: " + errorString);
+        }
+
+        return code;
+    }
+}
