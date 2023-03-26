4dc609bc8c223e64d5c6262da05dc2e89b6a79b4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-25 23:46:04 -0400

    Add all that good glfw initialization stuff

--- src/main/java/org/crafter/Engine/Window.java
+++ src/main/java/org/crafter/Engine/Window.java
@@ -1,9 +1,14 @@
 package org.crafter.Engine;
 
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
 
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 /**
  * Window interops with OpenGL 4.1 and GLFW 3.3.
@@ -24,6 +29,57 @@ public final class Window {
         // Using default callback
         GLFWErrorCallback.createPrint(System.err).set();
 
+        // Now initialize glfw
+        if (!glfwInit()) {
+            throw new RuntimeException("Window: Failed to initialize GLFW!");
+        }
+
+        // Let's just make sure the defaults are there
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(300, 300, "Hello dere!", NULL, NULL);
+
+        // Uh oh
+        if (window == NULL) {
+            throw new RuntimeException("Window: Failed to create the GLFW window!");
+        }
+
+        // Fancy key callback
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            System.out.println("Ohai dere");
+        });
+
+        // This song and dance is to center the window
+        try (MemoryStack stack = stackPush()) {
+
+            // These are C pointers
+            IntBuffer windowWidth  = stack.mallocInt(1);
+            IntBuffer windowHeight = stack.mallocInt(1);
+
+            // Intaking refs to these pointers (&windowWidth, etc)
+            glfwGetWindowSize(window, windowWidth, windowHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Now we can finally center it! Woo
+            glfwSetWindowPos(
+                    window,
+                    (videoMode.width() - windowWidth.get(0)) / 2,
+                    (videoMode.height() - windowHeight.get(0)) / 2
+            );
+        } // Out of the stack we go
+
+        // Omg, my window!
+        glfwMakeContextCurrent(window);
+
+        // But slow down there, bub
+        glfwSwapInterval(1);
+
+        // Now we can see it, yay!
+        glfwShowWindow(window);
 
     }
 
