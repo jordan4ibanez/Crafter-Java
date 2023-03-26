111a0492c2f555ccaecdd1081fcdd2dd60b2e476
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-26 04:44:05 -0400

    Errors work :)

--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -5,9 +5,12 @@ import org.crafter.engine.shader.ShaderStorage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         Window.initialize();
+
+
+        ShaderStorage.createShader("basic", "shaders/vertex.vert", "shaders/fragment.frag");
+
 
         while(!Window.shouldClose()) {
             Window.pollEvents();
