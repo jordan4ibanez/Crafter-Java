7bbec142219502dfde38c32865f8b962bf26034b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-27 18:23:33 -0400

    Base framework

--- src/main/java/org/crafter/engine/mesh/Mesh.java
+++ src/main/java/org/crafter/engine/mesh/Mesh.java
@@ -1,4 +1,29 @@
 package org.crafter.engine.mesh;
 
+/**
+ * The actual Mesh object.
+ * To interface into this you must talk to MeshStorage!
+ */
 public class Mesh {
+
+    // Reserved invalidation token
+    private static final int INVALID = Integer.MAX_VALUE;
+
+    // Required VAO & VBO
+    private final int vaoID;
+    private final int textureCoordinatesVboID;
+
+    private final int indicesVboID;
+
+    // Optional Vertex Buffer Objects
+    private int bonesVboID = INVALID;
+
+    private int colorsVboID = INVALID;
+
+    Mesh() {
+
+    }
+
+
+
 }
