7fc03dbbe0bb1bea815b9baf8e4e07ad73665f1c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-27 04:17:36 -0400

    Create TextureStorage.java

---
+++ src/main/java/org/crafter/engine/texture/TextureStorage.java
@@ -0,0 +1,16 @@
+package org.crafter.engine.texture;
+
+/**
+ * This class holds all the textures!
+ * A neat little house for them.
+ */
+public final class TextureStorage {
+
+    private TextureStorage(){}
+
+
+
+
+
+
+}
