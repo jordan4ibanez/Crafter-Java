15cfb16b8cbde0915357ab52ea50ec31c9431bd0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-27 04:23:57 -0400

    Add framework

--- src/main/java/org/crafter/engine/texture/TextureStorage.java
+++ src/main/java/org/crafter/engine/texture/TextureStorage.java
@@ -1,16 +1,42 @@
 package org.crafter.engine.texture;
 
+import java.util.HashMap;
+
 /**
  * This class holds all the textures!
  * A neat little house for them.
  */
 public final class TextureStorage {
 
+    // Here's where all the texures live!
+    private static final HashMap<String, Texture> container = new HashMap<>();
+
     private TextureStorage(){}
 
+    public static void createTexture(String fileLocation) {
+        if (container.containsKey(fileLocation)) {
+            throw new RuntimeException("TextureStorage: Tried to add " + fileLocation + " more than once!");
+        }
+        container.put(fileLocation, new Texture(fileLocation));
+    }
 
 
 
 
 
+
+
+
+    public static void destroy() {
+        for (Texture texture : container.values()) {
+            texture.destroy();
+        }
+    }
+
+    // Internal check to make sure nothing stupid is happening
+    private static void checkExistence(String textureName) {
+        if (!container.containsKey(textureName)) {
+            throw new RuntimeException("TextureStorage: Tried to access nonexistent texture (" + textureName + "!");
+        }
+    }
 }
