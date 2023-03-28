d2c2bb9fbcf2d4e5fe77e91044d6270c4ebf557b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-28 19:29:19 -0400

    Framework

--- src/main/java/org/crafter/engine/camera/Camera.java
+++ src/main/java/org/crafter/engine/camera/Camera.java
@@ -1,5 +1,21 @@
 package org.crafter.engine.camera;
 
-public class Camera {
-    //TODO: Standard ol' OpenGL camera
+import org.joml.Matrix4f;
+
+/**
+ * For now - There can only be one camera.
+ * In the future - Perhaps this will be a generic object
+ * with a CameraStorage static class to accompany it.
+ */
+public final class Camera {
+
+
+    Matrix4f cameraMatrix = new Matrix4f();
+    Matrix4f objectMatrix = new Matrix4f();
+
+    private Camera(){};
+
+    
+
+
 }
