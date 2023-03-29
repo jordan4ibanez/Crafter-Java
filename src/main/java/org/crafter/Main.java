35a6ec71b15ad295f926da0b67bc8c54e669f6be
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-28 21:10:53 -0400

    Now render a gray window

--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -1,5 +1,6 @@
 package org.crafter;
 
+import org.crafter.engine.camera.Camera;
 import org.crafter.engine.mesh.MeshStorage;
 import org.crafter.engine.texture.TextureStorage;
 import org.crafter.engine.window.Window;
@@ -14,15 +15,20 @@ public class Main {
         ShaderStorage.createShader("basic", "shaders/vertex.vert", "shaders/fragment.frag");
         ShaderStorage.createUniform("basic", new String[]{"cameraMatrix", "objectMatrix"});
 
-
         ShaderStorage.start("basic");
 
-
+        Window.setClearColor(0.75f);
 
 
         while(!Window.shouldClose()) {
 
             Window.pollEvents();
+
+            Window.clearAll();
+
+            Camera.updateCameraMatrix();
+
+
 
 
 
