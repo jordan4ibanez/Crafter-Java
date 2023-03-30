5cac9358902adac5281792bcbcbae898d0e75d88
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-29 20:43:50 -0400

    Create Text.java

---
+++ src/main/java/org/crafter/engine/gui/components/Text.java
@@ -0,0 +1,16 @@
+package org.crafter.engine.gui.components;
+
+/**
+ * Holds text data in memory.
+ */
+public class Text {
+    private String data;
+
+    private float fontSize;
+
+    Text (String data, float fontSize) {
+        this.data = data;
+        this.fontSize = fontSize;
+    }
+
+}
