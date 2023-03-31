2f05f78cb7a77b12b2f5e8db4ef6e48f15f3ea5e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-31 02:22:55 -0400

    Button is a gui element

---
+++ src/main/java/org/crafter/engine/gui/components/Button.java
@@ -0,0 +1,16 @@
+package org.crafter.engine.gui.components;
+
+import org.crafter.engine.gui.actions.Click;
+import org.crafter.engine.gui.actions.GUIElement;
+import org.crafter.engine.gui.actions.Hover;
+import org.crafter.engine.gui.actions.KeyInput;
+
+public class Button extends GUIElement {
+
+    private Button(){}
+
+    public Button(Hover hover, Click click, KeyInput keyInput) {
+        super(hover, click, keyInput);
+    }
+
+}
