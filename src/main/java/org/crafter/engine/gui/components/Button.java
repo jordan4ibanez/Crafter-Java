ac27d717c86510b17f41d216e8d62b4db1eea3a0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-31 04:26:06 -0400

    Implement new pattern for button & label

--- src/main/java/org/crafter/engine/gui/components/Button.java
+++ src/main/java/org/crafter/engine/gui/components/Button.java
@@ -1,12 +1,40 @@
 package org.crafter.engine.gui.components;
 
+import org.crafter.engine.gui.GUI;
 import org.crafter.engine.gui.actions.Click;
 import org.crafter.engine.gui.actions.Hover;
 import org.crafter.engine.gui.actions.KeyInput;
 import org.crafter.engine.gui.alignment.Alignment;
 
 public class Button extends GUIElement {
-    public Button(Hover hover, Click click, KeyInput keyInput, Alignment alignment) {
-        super(hover, click, keyInput, alignment);
+
+    public Button(Alignment alignment) {
+        super(alignment);
     }
+
+    @Override
+    public void onStep(GUI gui) {
+        
+    }
+
+    @Override
+    public void hover(GUI gui) {
+
+    }
+
+    @Override
+    public void click(GUI gui) {
+
+    }
+
+    @Override
+    public void keyInput(GUI gui) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
 }
--- src/main/java/org/crafter/engine/gui/components/Label.java
+++ src/main/java/org/crafter/engine/gui/components/Label.java
@@ -1,5 +1,6 @@
 package org.crafter.engine.gui.components;
 
+import org.crafter.engine.gui.GUI;
 import org.crafter.engine.gui.alignment.Alignment;
 
 /**
@@ -11,9 +12,33 @@ public class Label extends GUIElement {
     private float fontSize;
 
     Label(String data, float fontSize, Alignment alignment) {
-        super(null, null, null, alignment);
+        super(alignment);
         this.data = data;
         this.fontSize = fontSize;
     }
 
+    @Override
+    public void onStep(GUI gui) {
+
+    }
+
+    @Override
+    public void hover(GUI gui) {
+
+    }
+
+    @Override
+    public void click(GUI gui) {
+
+    }
+
+    @Override
+    public void keyInput(GUI gui) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
 }
