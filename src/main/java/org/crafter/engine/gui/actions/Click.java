b4c31cefcb8d5555d7ea14642a2167f1d8dd9f16
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-31 06:01:38 -0400

    Add final thing for the night

--- src/main/java/org/crafter/engine/gui/actions/Click.java
+++ src/main/java/org/crafter/engine/gui/actions/Click.java
@@ -1,7 +1,8 @@
 package org.crafter.engine.gui.actions;
 
 import org.crafter.engine.gui.GUI;
+import org.crafter.engine.gui.components.GUIElement;
 
 public interface Click {
-    void action(GUI gui);
+    void action(GUI gui, GUIElement element);
 }
--- src/main/java/org/crafter/engine/gui/actions/KeyInput.java
+++ src/main/java/org/crafter/engine/gui/actions/KeyInput.java
@@ -1,7 +1,8 @@
 package org.crafter.engine.gui.actions;
 
 import org.crafter.engine.gui.GUI;
+import org.crafter.engine.gui.components.GUIElement;
 
 public interface KeyInput {
-    void action(GUI gui, int keyboardKey);
+    void action(GUI gui, GUIElement element, int keyboardKey);
 }
