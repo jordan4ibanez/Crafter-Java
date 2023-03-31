a27bb9fe4b0ab958d988d86318da8401c5e3b537
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-31 03:55:08 -0400

    Add some more framework to GUIStorage

--- src/main/java/org/crafter/engine/gui/GUIStorage.java
+++ src/main/java/org/crafter/engine/gui/GUIStorage.java
@@ -6,8 +6,31 @@ public final class GUIStorage {
 
     private final static HashMap<String, GUI> container = new HashMap<>();
 
+    private static GUI selectedGUI;
+
     private GUIStorage(){};
 
 
-    
+
+    public static void addGUI(String guiName, GUI newGUI) {
+        checkDuplicates(guiName);
+        container.put(guiName, newGUI);
+    }
+
+    public static void selectGUI(String guiName) {
+        existenceCheck(guiName);
+        selectedGUI = container.get(guiName);
+    }
+
+
+    private static void existenceCheck(String guiName) {
+        if (!container.containsKey(guiName)) {
+            throw new RuntimeException("GUIStorage: ERROR! Tried to select nonexistent GUI (" + guiName + ")!");
+        }
+    }
+    private static void checkDuplicates(String guiName) {
+        if (container.containsKey(guiName)) {
+            throw new RuntimeException("GUIStorage: ERROR! Tried to add in (" + guiName + ") more than once!");
+        }
+    }
 }
