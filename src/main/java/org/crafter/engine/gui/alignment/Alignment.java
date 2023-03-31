5c20290c45d04490dfa8b07acfab825591a104d8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-31 03:28:11 -0400

    Create Alignment.java

---
+++ src/main/java/org/crafter/engine/gui/alignment/Alignment.java
@@ -0,0 +1,27 @@
+package org.crafter.engine.gui.alignment;
+
+import org.joml.Vector2f;
+
+public enum Alignment {
+    TOP_LEFT(new Vector2f(0.0f,0.0f)),
+    TOP_CENTER(new Vector2f(0.5f,0.0f)),
+    TOP_RIGHT(new Vector2f(1.0f,0.0f)),
+
+    CENTER_LEFT(new Vector2f(0.0f,0.5f)),
+    CENTER(new Vector2f(0.5f,0.5f)),
+    CENTER_RIGHT(new Vector2f(1.0f,0.5f)),
+
+    BOTTOM_LEFT(new Vector2f(0.0f,1.0f)),
+    BOTTOM_CENTER(new Vector2f(0.5f,1.0f)),
+    BOTTOM_RIGHT(new Vector2f(1.0f,1.0f));
+
+    final Vector2f value;
+
+    Alignment(Vector2f value) {
+        this.value = value;
+    }
+
+    public Vector2f value() {
+        return this.value;
+    }
+}
