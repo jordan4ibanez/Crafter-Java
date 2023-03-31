cc453335b2fca044b503635efc6efbf2d7959503
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-03-31 02:37:21 -0400

    Text does nothing

--- src/main/java/org/crafter/engine/gui/components/Text.java
+++ src/main/java/org/crafter/engine/gui/components/Text.java
@@ -11,6 +11,7 @@ public class Text extends GUIElement {
     private float fontSize;
 
     Text (String data, float fontSize) {
+        super(null, null, null);
         this.data = data;
         this.fontSize = fontSize;
     }
