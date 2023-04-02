e0c058a851cead9724250cde8647c7eba1b1c690
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-01 22:16:27 -0400

    Make a factory

---
+++ src/main/java/org/crafter/engine/gui/factories/ButtonMeshFactory.java
@@ -0,0 +1,15 @@
+package org.crafter.engine.gui.factories;
+
+public final class ButtonMeshFactory {
+
+    private ButtonMeshFactory(){}
+
+    /**
+     * Button Mesh Factory does exactly what it says on the tin.
+     * It's sole existence is to generate the mesh for the Button component.
+     * This keeps the Button class clean as a whistle.
+     */
+    public void generateMesh() {
+        System.out.println("todo");
+    }
+}
