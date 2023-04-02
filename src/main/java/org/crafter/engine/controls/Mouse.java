a6d0379a914aeb912b9022e3e1390b729d910151
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-02 18:01:08 -0400

    Set some basic callbacks

--- src/main/java/org/crafter/engine/controls/Mouse.java
+++ src/main/java/org/crafter/engine/controls/Mouse.java
@@ -1,4 +1,41 @@
 package org.crafter.engine.controls;
 
-public class Mouse {
+import org.crafter.engine.window.Window;
+import org.joml.Vector2f;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public final class Mouse {
+    private static final Vector2f position = new Vector2f(-1,-1);
+
+    private static boolean leftClick = false;
+    private static boolean rightClick = false;
+
+    private Mouse(){}
+
+    public static void initialize() {
+
+        if (glfwRawMouseMotionSupported()) {
+            glfwSetInputMode(Window.getWindowPointer(), GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+        }
+
+        glfwSetCursorPosCallback(Window.getWindowPointer(),(windowPointer, xPos, yPos) -> {
+            position.set(xPos, yPos);
+        });
+
+        glfwSetCursorEnterCallback(Window.getWindowPointer(), (windowPointer, entered) -> {
+            // Only resetting to -1 when the mouse leaves
+            if (!entered) {
+                position.set(-1, -1);
+            }
+        });
+    }
+
+    public static void capture() {
+        glfwSetInputMode(Window.getWindowPointer(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+
+    public static void release() {
+        glfwSetInputMode(Window.getWindowPointer(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
 }
