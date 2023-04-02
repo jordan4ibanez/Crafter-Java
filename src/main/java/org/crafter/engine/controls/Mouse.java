d64db86a4764358f36f8d8817d694b055411f706
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-02 18:12:19 -0400

    More framework

--- src/main/java/org/crafter/engine/controls/Mouse.java
+++ src/main/java/org/crafter/engine/controls/Mouse.java
@@ -9,7 +9,12 @@ public final class Mouse {
     private static final Vector2f position = new Vector2f(-1,-1);
 
     private static boolean leftClick = false;
+    private static boolean leftHeld = false;
+    private static boolean leftWasHeld = false;
+
     private static boolean rightClick = false;
+    private static boolean rightHeld = false;
+    private static boolean rightWasHeld = false;
 
     private Mouse(){}
 
@@ -29,6 +34,29 @@ public final class Mouse {
                 position.set(-1, -1);
             }
         });
+    }
+
+    // This needs to be polled like this to simultaneously poll mouse button down & held
+    public static void poll() {
+        int leftButtonState = glfwGetMouseButton(Window.getWindowPointer(), GLFW_MOUSE_BUTTON_LEFT);
+        int rightButtonState = glfwGetMouseButton(Window.getWindowPointer(), GLFW_MOUSE_BUTTON_RIGHT);
+
+        if (leftButtonState == GLFW_PRESS) {
+            if (!leftWasHeld) {
+                leftClick = true;
+                System.out.println("left click!");
+            } else {
+                leftClick = false;
+            }
+            // Now disable click on next step
+            if (leftHeld) {
+                leftWasHeld = true;
+            }
+            leftHeld = true;
+        }
+
+
+
     }
 
     public static void capture() {
