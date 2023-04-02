afc1f75ed34edcca0590f37d2dd8473c7decef48
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-02 18:36:54 -0400

    Set some basic framework for keyboard

--- src/main/java/org/crafter/engine/controls/Keyboard.java
+++ src/main/java/org/crafter/engine/controls/Keyboard.java
@@ -1,4 +1,21 @@
 package org.crafter.engine.controls;
 
-public class Keyboard {
+import org.crafter.engine.window.Window;
+
+import static org.lwjgl.glfw.GLFW.glfwSetCharCallback;
+
+public final class Keyboard {
+
+    // Starts of as a C line terminator
+    private static char lastKey = '\0';
+
+    private Keyboard(){}
+
+    public static void initialize() {
+        glfwSetCharCallback(Window.getWindowPointer(), (window, codePoint) ->{
+            System.out.println(codePoint);
+        });
+    }
+
+
 }
