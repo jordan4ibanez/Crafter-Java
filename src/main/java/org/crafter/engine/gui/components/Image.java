854c57dec2d44e0c3a92f13a3fb4c3bebfefc77f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-03 21:54:47 -0400

    Implement some base framework

---
+++ src/main/java/org/crafter/engine/gui/components/Image.java
@@ -0,0 +1,72 @@
+package org.crafter.engine.gui.components;
+
+import org.crafter.engine.gui.GUI;
+import org.crafter.engine.gui.enumerators.Alignment;
+import org.crafter.engine.gui.factories.ImageMeshFactory;
+import org.crafter.engine.mesh.MeshStorage;
+import org.crafter.engine.texture.TextureStorage;
+import org.joml.Vector2f;
+import org.joml.Vector2fc;
+
+public class Image extends GUIElement{
+
+    private final String fileLocation;
+
+    // Used to keep aspect ratio of raw image
+    private final Vector2f originalImageSize = new Vector2f(0);
+
+    // Used to keep aspect ratio of trimmed image
+    private final Vector2f trimmedImageSize = new Vector2f(0);
+
+    private float scale;
+
+    private boolean trimmingEnabled = false;
+
+    protected Image(String fileLocation, float scale, Alignment alignment, Vector2f offset) {
+        super(alignment, offset);
+
+        this.fileLocation = fileLocation;
+        this.scale = scale;
+
+        originalImageSize.set(TextureStorage.getFloatingSize(fileLocation));
+
+        recalculateMesh();
+    }
+
+    public void enableTrimming() {
+        if (trimmingEnabled) {
+            throw new RuntimeException("Image: You tried to enable trimming more than once!");
+        }
+        trimmingEnabled = true;
+        recalculateMesh();
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public boolean collisionDetect(Vector2fc mousePosition) {
+        return false;
+    }
+
+    @Override
+    protected void recalculateMesh() {
+        if (_meshUUID != null) {
+            MeshStorage.destroy(_meshUUID);
+        }
+
+        _meshUUID = ImageMeshFactory.createImageMesh(scale);
+    }
+
+    @Override
+    public void internalOnStep(GUI gui) {
+
+    }
+
+    @Override
+    protected void recalculatePosition() {
+
+    }
+}
---
+++ src/main/java/org/crafter/engine/gui/factories/ImageMeshFactory.java
@@ -0,0 +1,15 @@
+package org.crafter.engine.gui.factories;
+
+import org.joml.Vector2fc;
+
+public final class ImageMeshFactory {
+
+
+
+    private ImageMeshFactory(){}
+
+    public static String createImageMesh(float scale) {
+
+        return "test";
+    }
+}
