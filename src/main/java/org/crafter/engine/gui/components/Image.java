9673e988bd120c4c9baa140ccbf4f868d1e878ab
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-03 23:51:27 -0400

    Push current changes to Image

--- src/main/java/org/crafter/engine/gui/components/Image.java
+++ src/main/java/org/crafter/engine/gui/components/Image.java
@@ -1,10 +1,12 @@
 package org.crafter.engine.gui.components;
 
+import org.crafter.engine.camera.Camera;
 import org.crafter.engine.gui.GUI;
 import org.crafter.engine.gui.enumerators.Alignment;
 import org.crafter.engine.gui.factories.ImageMeshFactory;
 import org.crafter.engine.mesh.MeshStorage;
 import org.crafter.engine.texture.TextureStorage;
+import org.crafter.engine.window.Window;
 import org.joml.Vector2f;
 import org.joml.Vector2fc;
 
@@ -13,7 +15,7 @@ public class Image extends GUIElement{
     private final String fileLocation;
 
     // Used to keep aspect ratio of raw image
-    private final Vector2f originalImageSize = new Vector2f(0);
+    private final Vector2fc originalImageSize;
 
     // Used to keep aspect ratio of trimmed image
     private final Vector2f trimmedImageSize = new Vector2f(0);
@@ -22,13 +24,13 @@ public class Image extends GUIElement{
 
     private boolean trimmingEnabled = false;
 
-    protected Image(String fileLocation, float scale, Alignment alignment, Vector2f offset) {
+    public Image(String fileLocation, float scale, Alignment alignment, Vector2f offset) {
         super(alignment, offset);
 
         this.fileLocation = fileLocation;
         this.scale = scale;
 
-        originalImageSize.set(TextureStorage.getFloatingSize(fileLocation));
+        originalImageSize = TextureStorage.getFloatingSize(fileLocation);
 
         recalculateMesh();
     }
@@ -41,13 +43,20 @@ public class Image extends GUIElement{
         recalculateMesh();
     }
 
+    public void scale(float newScale) {
+        this.scale = newScale;
+        recalculateMesh();
+    }
+
     @Override
     public void render() {
-
+        Camera.setGuiObjectMatrix(_position.x, _position.y);
+        MeshStorage.render(this._meshUUID);
     }
 
     @Override
     public boolean collisionDetect(Vector2fc mousePosition) {
+        // Does nothing
         return false;
     }
 
@@ -57,16 +66,25 @@ public class Image extends GUIElement{
             MeshStorage.destroy(_meshUUID);
         }
 
-        _meshUUID = ImageMeshFactory.createImageMesh(scale, fileLocation);
+        _meshUUID = ImageMeshFactory.createImageMesh(scale * getGuiScale(), fileLocation);
+
+        this._size.set(originalImageSize.x() * scale * getGuiScale(), originalImageSize.y() * scale * getGuiScale());
+
+        recalculatePosition();
     }
 
     @Override
     public void internalOnStep(GUI gui) {
-
+        if (Window.wasResized()) {
+            recalculateMesh();
+        }
     }
 
     @Override
     protected void recalculatePosition() {
-
+        this._position.set(_alignment.value().mul(Window.getWindowSize()).sub(getSize().mul(_alignment.value())).add(offset()));
+        System.out.println("Image (" + this.name() + ") POSITION: " + _position.x + ", " + _position.y);
     }
+
+
 }
