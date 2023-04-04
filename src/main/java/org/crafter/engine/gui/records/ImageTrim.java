d552f2bca999d16a9c997115c4fee6d1b53c62dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-04 18:55:50 -0400

    Create a custom record

---
+++ src/main/java/org/crafter/engine/gui/records/ImageTrim.java
@@ -0,0 +1,10 @@
+package org.crafter.engine.gui.records;
+
+/**
+ * Left, Right, Top, Bottom
+ * @param startX
+ * @param endX
+ * @param startY
+ * @param endY
+ */
+public record ImageTrim(float width, float height, float startX, float endX, float startY, float endY){}
