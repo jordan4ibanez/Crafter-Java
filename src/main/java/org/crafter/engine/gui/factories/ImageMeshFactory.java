f04677c67ef18d987967fadfabe8033440939de1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-03 23:12:22 -0400

    Push out some cool textures

--- src/main/java/org/crafter/engine/gui/factories/ImageMeshFactory.java
+++ src/main/java/org/crafter/engine/gui/factories/ImageMeshFactory.java
@@ -1,10 +1,16 @@
 package org.crafter.engine.gui.factories;
 
+import org.crafter.engine.mesh.MeshStorage;
+import org.crafter.engine.texture.TextureStorage;
 import org.joml.Vector2f;
 import org.joml.Vector2fc;
 
 import java.util.HashMap;
+import java.util.UUID;
 
+/**
+ * This generates a rectangle mesh image. That's it.
+ */
 public final class ImageMeshFactory {
 
     // Reuser field
@@ -20,6 +26,48 @@ public final class ImageMeshFactory {
 
         Vector2fc imageTextureSize = textureSizes.get(fileLocation);
 
-        return "test";
+        if (imageTextureSize == null) {
+            imageTextureSize = TextureStorage.getFloatingSize(fileLocation);
+
+            textureSizes.put(fileLocation, imageTextureSize);
+        }
+
+        final float[] vertices = new float[]{
+                0.0f,                 0.0f,
+                0.0f,                 imageTextureSize.y(),
+                imageTextureSize.x(), imageTextureSize.y(),
+                imageTextureSize.x(), 0.0f,
+        };
+
+        final float[] textureCoordinates = new float[]{
+                0.0f, 0.0f,
+                0.0f, 1.0f,
+                1.0f, 1.0f,
+                1.0f, 0.0f
+        };
+
+        final int[] indices = new int[]{
+                0,1,2,3
+        };
+
+        // Fully blank, the shader takes care of blank color space
+        final float[] colors = new float[12];
+
+        String uuid = UUID.randomUUID().toString();
+
+        MeshStorage.newMesh(
+                uuid,
+                vertices,
+                textureCoordinates,
+                indices,
+                null,
+                colors,
+                fileLocation,
+                true
+        );
+
+        System.out.println("ImageMeshFactory: Shipping out UUID (" + uuid + ")!");
+
+        return uuid;
     }
 }
