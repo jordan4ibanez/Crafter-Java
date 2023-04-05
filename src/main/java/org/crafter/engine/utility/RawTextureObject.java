4b932dac91fcc8fcd1c962ac71ee83726b315fa1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-04 20:46:17 -0400

    Fix a major oversight

--- src/main/java/org/crafter/engine/utility/RawTextureObject.java
+++ src/main/java/org/crafter/engine/utility/RawTextureObject.java
@@ -17,6 +17,8 @@ public class RawTextureObject {
     final int width;
     final int height;
 
+    final int channels;
+
     final ByteBuffer buffer;
 
     public RawTextureObject(String fileLocation) {
@@ -25,10 +27,10 @@ public class RawTextureObject {
             IntBuffer stackWidth = stack.mallocInt(1);
             IntBuffer stackHeight = stack.mallocInt(1);
 
-            IntBuffer channels = stack.mallocInt(1);
+            IntBuffer stackChannels = stack.mallocInt(1);
 
             // Desired channels = 4 = R,G,B,A
-            buffer = stbi_load(fileLocation, stackWidth, stackHeight, channels, 4);
+            buffer = stbi_load(fileLocation, stackWidth, stackHeight, stackChannels, 4);
 
             if (buffer == null) {
                 throw new RuntimeException("RawTextureObject: Failed to load (" + fileLocation + ")! Error: " + stbi_failure_reason());
@@ -36,6 +38,7 @@ public class RawTextureObject {
 
             width = stackWidth.get(0);
             height = stackHeight.get(0);
+            channels = stackChannels.get(0);
         }
     }
 
@@ -45,6 +48,10 @@ public class RawTextureObject {
 
     public int getHeight() {
         return height;
+    }
+
+    public int getChannels() {
+        return channels;
     }
 
     public ByteBuffer getBuffer() {
@@ -77,10 +84,10 @@ public class RawTextureObject {
         // Now return it as a JOML vec4i
         return new Vector4i(
                 // & 0xff to make it a true ubyte in java's int, otherwise, it's garbage data
-                buffer.getInt(index) & 0xff,
-                buffer.getInt(index + 1) & 0xff,
-                buffer.getInt(index + 2) & 0xff,
-                buffer.getInt(index + 3) & 0xff
+                (int)buffer.get(index) & 0xff,
+                (int)buffer.get(index + 1) & 0xff,
+                (int)buffer.get(index + 2) & 0xff,
+                (int)buffer.get(index + 3) & 0xff
         );
     }
 
