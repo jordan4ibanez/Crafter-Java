5f4d200fb900bc3adf7591b8ca0845b40e629060
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-05 03:08:22 -0400

    Some basic checks and whatnot

--- src/main/java/org/crafter/engine/gui/components/DropMenu.java
+++ src/main/java/org/crafter/engine/gui/components/DropMenu.java
@@ -7,14 +7,24 @@ import org.joml.Vector2fc;
 
 public class DropMenu extends GUIElement {
 
+    // We don't want the size to change basically
+    private final Vector2fc boxSize;
     private int selectedOption = 0;
+
+    private final String[] options;
 
     protected DropMenu(Vector2f boxSize, String[] options, float fontSize, Alignment alignment, Vector2f offset) {
         super(alignment, offset);
 
+        if (boxSize == null) {
+            throw new RuntimeException("DropMenu: You must specify a boxSize for your drop menu!");
+        }
         if (options.length < 2) {
             throw new RuntimeException("DropMenu: You must have more than one option in your drop menu!");
         }
+        this.boxSize = boxSize;
+        this.options = options;
+
     }
 
     public void setCurrentOption() {
