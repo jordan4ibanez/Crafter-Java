c336a5155b235f44943f7f3d69fdd5ae4efa1258
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-04 23:23:02 -0400

    Add more comments

--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -139,8 +139,8 @@ public class Main {
                      )
                      .addGUIElement("imageBoi", new Image("textures/debug.png", 10, Alignment.CENTER_LEFT, null)
                      )
-                     // -160 because original is 16x16 scaled by 10x
-                     .addGUIElement("trimTest", new Image("textures/trim_test.png", 10, Alignment.CENTER_LEFT, new Vector2f(0,-160), true)
+                     // -160 because original is 16x16 scaled by 10x, trimmed pixels (1 off top, 1 off bottom) makes it 3x14
+                     .addGUIElement("trimTest", new Image("textures/trim_test.png", 10, Alignment.CENTER_LEFT, new Vector2f(0,-150), true)
                      )
 //                     .addGUIElement(
 //                             "sassyButton",
