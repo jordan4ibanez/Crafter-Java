a4d730532eecfc61103b57765f54f60f6e6cbf30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-09 04:01:27 -0400

    Test Discord bot

--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -19,6 +19,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("hi");
+
         Window.initialize();
         Window.setTitle(getVersionInfo(), true);
 
