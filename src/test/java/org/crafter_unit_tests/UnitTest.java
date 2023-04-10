007f243fe753f2e2dece39343310d38be1e20ecc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-10 02:45:47 -0400

    Now implement the array test

--- src/test/java/org/crafter_unit_tests/UnitTest.java
+++ src/test/java/org/crafter_unit_tests/UnitTest.java
@@ -1,16 +1,24 @@
 package org.crafter_unit_tests;
 
 import org.crafter.engine.chunk.Chunk;
+import org.joml.Vector3ic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
     @Test
-    public void testChunkBitShifting() {
+    public void testChunkArrayIndexing() {
         Chunk testChunk = new Chunk(0,0);
-        for (int i = 0; i <= 65_535; i++) {
-            // assertEquals(testChunk.getBlockID(testChunk.setBlockID(0, i)), i);
+        for (int i = 0; i < testChunk.getArraySize(); i++) {
+
+            Vector3ic position = testChunk.indexToPosition(i);
+//            System.out.println(position.x() + ", " + position.y() + ", " + position.z());
+            int index = testChunk.positionToIndex(position);
+//            System.out.println("actual: " + i + " | gotten: " + index);
+            assertEquals(index, i);
+            Vector3ic doubleCheck = testChunk.indexToPosition(index);
+            assertEquals(doubleCheck, position);
         }
     }
 }
