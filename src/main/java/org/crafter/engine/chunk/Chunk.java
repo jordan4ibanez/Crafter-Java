f61234ca84809868ef0f08d1e7d4cd46a15baeaf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-10 00:26:05 -0400

    Add notes

--- src/main/java/org/crafter/engine/chunk/Chunk.java
+++ src/main/java/org/crafter/engine/chunk/Chunk.java
@@ -6,11 +6,18 @@ import org.joml.Vector2ic;
 
 /**
  * A chunk of map data. It is 16 wide, 128 high, and 16 long.
+ *
+ * Some notes:
+ * << shifts to the left X amount, so << 3 of 0001 in a byte now represents 1000
+ * >> shifts to the right X amount
+ * 
  */
 public class Chunk {
 
     private static final int arraySize = 16*16*128;
     private final Vector2ic position;
+
+
 
     // Consists of bit shifted integral values
     private final int[] data;
@@ -41,7 +48,7 @@ public class Chunk {
     //Todo: bitshift light, block id, state
 
     public void debugZero() {
-        int test = 3;
+        int test = 3 << 3;
 
         NumberTools.printBits(test);
     }
--- src/main/java/org/crafter/engine/utility/NumberTools.java
+++ src/main/java/org/crafter/engine/utility/NumberTools.java
@@ -3,6 +3,7 @@ package org.crafter.engine.utility;
 public final class NumberTools {
     private static final StringBuilder output = new StringBuilder();
     private NumberTools(){}
+
     public static void printBits(int input) {
         for (int i = 31; i >= 0; i--) {
             output.append((input & (1 << i)) == 0 ? "0" : "1");
