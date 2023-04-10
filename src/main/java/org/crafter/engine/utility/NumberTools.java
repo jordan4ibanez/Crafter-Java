6bcb86c6e538f02799857632b555468d9478bd00
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-10 00:23:01 -0400

    Create NumberTools.java

---
+++ src/main/java/org/crafter/engine/utility/NumberTools.java
@@ -0,0 +1,13 @@
+package org.crafter.engine.utility;
+
+public final class NumberTools {
+    private static final StringBuilder output = new StringBuilder();
+    private NumberTools(){}
+    public static void printBits(int input) {
+        for (int i = 31; i >= 0; i--) {
+            output.append((input & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println("Literal (" + input + ") | binary: " + output);
+        output.setLength(0);
+    }
+}
