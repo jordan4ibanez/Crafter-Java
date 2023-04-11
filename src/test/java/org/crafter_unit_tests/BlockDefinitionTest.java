0eadc18af371c8274b194e6863f3d1da7b5cf4e1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-10 21:33:20 -0400

    All the unit tests

--- src/test/java/org/crafter_unit_tests/BlockDefinitionTest.java
+++ src/test/java/org/crafter_unit_tests/BlockDefinitionTest.java
@@ -61,10 +61,86 @@ public class BlockDefinitionTest {
 
 //            System.out.println(gottenDrawType);
 
-            assertEquals(1,2);
+            final boolean chosenWalkable = random.nextInt(2) == 1;
+            testDefinition.setWalkable(chosenWalkable);
+            final boolean gottenWalkable = testDefinition.getWalkable();
 
+            assertEquals(chosenWalkable, gottenWalkable);
 
+//            System.out.println(gottenWalkable);
 
+            final boolean chosenLiquid = random.nextInt(2) == 1;
+            testDefinition.setLiquid(chosenLiquid);
+            final boolean gottenLiquid = testDefinition.getLiquid();
+
+            assertEquals(chosenLiquid, gottenLiquid);
+
+//            System.out.println(gottenLiquid);
+
+            final int chosenLiquidFLow = random.nextInt(8) + 1;
+            testDefinition.setLiquidFlow(chosenLiquidFLow);
+            final int gottenLiquidFlow = testDefinition.getLiquidFlow();
+
+            assertEquals(chosenLiquidFLow, gottenLiquidFlow);
+
+//            System.out.println(gottenLiquidFlow);
+
+            final int chosenLiquidViscosity = random.nextInt(8) + 1;
+            testDefinition.setLiquidViscosity(chosenLiquidViscosity);
+            final int gottenLiquidViscosity = testDefinition.getLiquidViscosity();
+
+            assertEquals(chosenLiquidViscosity, gottenLiquidViscosity);
+
+//            System.out.println(gottenLiquidViscosity);
+
+            final boolean chosenClimbable = random.nextInt(2) == 1;
+            testDefinition.setClimbable(chosenClimbable);
+            final boolean gottenClimbable = testDefinition.getClimbable();
+
+            assertEquals(chosenClimbable, gottenClimbable);
+
+//            System.out.println(gottenClimbable);
+
+            final boolean chosenSneakJumpClimbable = random.nextInt(2) == 1;
+            testDefinition.setSneakJumpClimbable(chosenSneakJumpClimbable);
+            final boolean gottenSneakJumpClimbable = testDefinition.getSneakJumpClimbable();
+
+            assertEquals(chosenSneakJumpClimbable, gottenSneakJumpClimbable);
+
+//            System.out.println(gottenSneakJumpClimbable);
+
+            final boolean chosenFalling = random.nextInt(2) == 1;
+            testDefinition.setFalling(chosenFalling);
+            final boolean gottenFalling = testDefinition.getFalling();
+
+            assertEquals(chosenFalling, gottenFalling);
+
+//            System.out.println(gottenFalling);
+
+            final boolean chosenClear = random.nextInt(2) == 1;
+            testDefinition.setClear(chosenClear);
+            final boolean gottenClear = testDefinition.getClear();
+
+            assertEquals(chosenClear, gottenClear);
+
+//            System.out.println(gottenClear);
+
+            // That's alotta damage!
+            final int chosenDamagePerSecond = random.nextInt(100) + 1;
+            testDefinition.setDamagePerSecond(chosenDamagePerSecond);
+            final int gottenDamagePerSecond = testDefinition.getDamagePerSecond();
+
+            assertEquals(chosenDamagePerSecond, gottenDamagePerSecond);
+
+//            System.out.println(gottenDamagePerSecond);
+
+            final int chosenLight = random.nextInt(15) + 1;
+            testDefinition.setLight(chosenLight);
+            final int gottenLight = testDefinition.getLight();
+
+            assertEquals(chosenLight, gottenLight);
+
+//            System.out.println(gottenLight);
         }
 
     }
