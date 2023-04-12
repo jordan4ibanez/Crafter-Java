362d091b1e1c95160161bb05b024d5a60949a043
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-12 04:49:23 -0400

    Set up error handling :D

--- src/main/java/org/crafter/engine/api/API.java
+++ src/main/java/org/crafter/engine/api/API.java
@@ -1,5 +1,6 @@
 package org.crafter.engine.api;
 
+import party.iroiro.luajava.Lua;
 import party.iroiro.luajava.luajit.LuaJit;
 
 
@@ -12,9 +13,18 @@ public final class API {
 
     public static void initialize() {
 
-        luaJIT.run(getFileString("api/api.lua"));
+        runFile("api/api.lua");
 
     }
 
+    private static void runFile(String fileLocation) {
+        checkError(luaJIT.run(getFileString(fileLocation)));
 
+    }
+
+    private static void checkError(Lua.LuaError error){
+        if (error != Lua.LuaError.OK) {
+            throw new RuntimeException(luaJIT.toString(-1));
+        }
+    }
 }
