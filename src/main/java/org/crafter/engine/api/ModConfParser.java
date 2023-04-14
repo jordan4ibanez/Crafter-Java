fc44ee0923b9d8910c84e421e8ee5c59441ff896
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-14 19:57:04 -0400

    Add a basic parser

--- src/main/java/org/crafter/engine/api/ModConfParser.java
+++ src/main/java/org/crafter/engine/api/ModConfParser.java
@@ -1,9 +1,13 @@
 package org.crafter.engine.api;
 
-import java.util.HashMap;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 
-import static org.crafter.engine.utility.FileReader.isFile;
-import static org.crafter.engine.utility.FileReader.isFolder;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import static org.crafter.engine.utility.FileReader.*;
 
 /**
  * Automatically parses and stored required values from a mod.json file.
@@ -34,6 +38,55 @@ public class ModConfParser {
          * TODO: Check checkDuplicateDirectValue when parsing singular values
          * TODO: Check checkDuplicateArrayValue when parsing array values (like dependencies)
          */
+    }
+
+    private void parseJson() {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode nodes;
+
+        try {
+            nodes = mapper.readTree(getFileString(jsonLocation));
+        } catch (Exception e) {
+            throw new RuntimeException("Font: ERROR loading! " + e);
+        }
+
+        // Crawl up the JSON tree
+
+        Iterator<String> keys = nodes.fieldNames();
+
+        for (Iterator<JsonNode> it = nodes.elements(); it.hasNext(); ) {
+
+            String key = keys.next();
+            JsonNode value = it.next();
+
+            JsonNodeType type = value.getNodeType();
+
+            switch (key) {
+                case "name" -> {
+                    assert (type == JsonNodeType.STRING);
+                    checkDuplicateDirectValue("name");
+                    directValues.put("name", value.asText());
+                }
+                case "version" -> {
+                    assert (type == JsonNodeType.STRING);
+                    checkDuplicateDirectValue("version");
+                    directValues.put("version", value.asText());
+                }
+                case "description" -> {
+                    assert (type == JsonNodeType.STRING);
+                    checkDuplicateDirectValue("description");
+                    directValues.put("version", value.asText());
+                }
+                case "dependencies" -> {
+                    assert (type == JsonNodeType.ARRAY);
+                    // TODO: PUT THE ARRAY OF STRINGS HERE
+                }
+                default -> {}
+            }
+
+        }
     }
 
     public boolean containsDirectValue(String key) {
