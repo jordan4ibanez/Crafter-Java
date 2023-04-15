3e4d5dbe92bfa474e8c0d131669722ac9602f8d0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-14 21:39:46 -0400

    Framework of ChunkGenerator

--- src/main/java/org/crafter/engine/world_generation/ChunkGenerator.java
+++ src/main/java/org/crafter/engine/world_generation/ChunkGenerator.java
@@ -1,5 +1,31 @@
 package org.crafter.engine.world_generation;
 
-public class ChunkGenerator {
+import org.crafter.engine.utility.FastNoise;
+import org.crafter.engine.world.block.BlockDefinitionContainer;
 
+import java.util.Arrays;
+
+public class ChunkGenerator implements Runnable {
+
+    private static final Thread thread = new Thread(new ChunkGenerator());
+
+    private final FastNoise noise;
+
+    private final BlockDefinitionContainer blockDefinitionContainer;
+
+    public ChunkGenerator() {
+        noise = new FastNoise();
+        blockDefinitionContainer = BlockDefinitionContainer.getThreadSafeDuplicate();
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("ChunkGenerator: Started!");
+        System.out.println("ChunkGenerator: gotten blocks (" + Arrays.toString(blockDefinitionContainer.getAllBlockNames()) + ")!");
+    }
+
+    public static void start() {
+        thread.start();
+    }
 }
