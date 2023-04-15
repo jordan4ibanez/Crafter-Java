73d3fb5b7b6181073f589f567525fe0a94355821
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-14 22:26:35 -0400

    Add current testing for thread, notes below
    Thread has been thoroughly tested. Original code was to use a game-like thread check, no pause, caused an entire core to become consumed.
    
    Sleep option is better, uses minimal cpu, can queue up multiple jobs during sleep process, can be fine tuned. 0.2 seconds has same cpu utilization at 0.5 seconds.
    
    Thread sleep seems to be ideal blend of responsiveness and power consumption.

--- src/main/java/org/crafter/engine/world_generation/ChunkGenerator.java
+++ src/main/java/org/crafter/engine/world_generation/ChunkGenerator.java
@@ -1,5 +1,6 @@
 package org.crafter.engine.world_generation;
 
+import org.crafter.engine.delta.DeltaObject;
 import org.crafter.engine.utility.FastNoise;
 import org.crafter.engine.world.block.BlockDefinitionContainer;
 import org.joml.Vector2ic;
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * The Chunk Generator is basically a thread facade.
@@ -20,18 +22,23 @@ public class ChunkGenerator implements Runnable {
     private static Thread thread;
 
     // Instance local
+    private final DeltaObject delta;
     private final FastNoise noise;
 
     private final BlockDefinitionContainer blockDefinitionContainer;
 
-    BlockingQueue<Vector2ic> chunkQueue;
-    AtomicBoolean shouldRun;
+    private final BlockingQueue<Vector2ic> chunkQueue;
+    private final AtomicBoolean shouldRun;
+
+//    private float sleepTimer;
 
     private ChunkGenerator() {
+        delta = new DeltaObject();
         noise = new FastNoise();
         blockDefinitionContainer = BlockDefinitionContainer.getThreadSafeDuplicate();
         chunkQueue = new LinkedBlockingQueue<>();
         shouldRun = new AtomicBoolean(true);
+//        sleepTimer = 0.0f;
     }
 
     @Override
@@ -39,8 +46,40 @@ public class ChunkGenerator implements Runnable {
         System.out.println("ChunkGenerator: Started!");
         System.out.println("ChunkGenerator: gotten blocks (" + Arrays.toString(blockDefinitionContainer.getAllBlockNames()) + ")!");
         while (shouldRun.get()) {
+            sleepCheck();
             System.out.println("ChunkGenerator: I am a runner");
         }
+    }
+
+    private void sleepCheck() {
+        /*
+         The side effect of this version is:
+         - Players will have to wait an entire 0.2 seconds (1/5th of second) until thread can begin processing the queue.
+         But, I think this is way, way more worth it as power figures ranged from:
+         - Sleep: 1.8% - 2.5%
+         - Delta poll: 18% - 23%
+        */
+        if (chunkQueue.size() == 0) {
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                throw new RuntimeException("ChunkGenerator: Thread failed to sleep! " + e);
+            }
+        }
+
+        /* This version is very cpu intensive! But it can be modified to be very responsive.
+        Idea: Maybe an optional thread sleep variable? Give warning that it will just chomp cpu to no end
+        if (chunkQueue.size() == 0) {
+            delta.calculateDelta();
+            sleepTimer += delta.getDelta();
+            if (sleepTimer < 0.5f) {
+                return true;
+            }
+            sleepTimer = 0.0f;
+            return false;
+        }
+        return false;
+         */
     }
 
     private void addRequest(Vector2ic requestedChunk) {
