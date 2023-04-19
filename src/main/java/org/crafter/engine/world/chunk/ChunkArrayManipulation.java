fce7596451dd2c937f3957ab81ff9ed7886c5382
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-18 23:28:39 -0400

    Add contextual information into ChunkArrayManipulation

--- src/main/java/org/crafter/engine/world/chunk/ChunkArrayManipulation.java
+++ src/main/java/org/crafter/engine/world/chunk/ChunkArrayManipulation.java
@@ -9,6 +9,18 @@ import java.util.Arrays;
  * The Basis for working with Chunk's internal Array.
  * Chunks are basically fancy arrays of data.
  * This class goes into Chunk, finalizing this snowball of inheritance.
+ *
+ *
+ * A chunk of map data. It is 16 wide, 128 high, and 16 long.
+ *
+ * Some notes:
+ * << shifts to the left X amount, so << 3 of 0001 (1) in a byte now represents 1000 (8)
+ * >> shifts to the right X amount
+ *
+ * Chunk represented as:
+ * [16 bit] block | [4 bit lightLevel] | [4 bit blockState] | [ 8 bits left over for additional functionality]
+ * This is literal, here is an exact representation:
+ * | 0000 0000 0000 0000 | 0000 | 0000 | 0000 0000 |
  */
 public abstract class ChunkArrayManipulation extends ChunkBitManipulation {
 
