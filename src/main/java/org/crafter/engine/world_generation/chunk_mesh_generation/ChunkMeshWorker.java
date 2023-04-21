275c9659f3bafecbdb2de5f9eaa02123e8628bf8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-21 18:15:27 -0400

    Turn off verbose debug test

--- src/main/java/org/crafter/engine/world_generation/chunk_mesh_generation/ChunkMeshWorker.java
+++ src/main/java/org/crafter/engine/world_generation/chunk_mesh_generation/ChunkMeshWorker.java
@@ -25,49 +25,49 @@ public class ChunkMeshWorker {
         // vertex points
 
         // top left
-        positions.add(-0.5f); // x
-        positions.add( 0.5f); // y
-        positions.add( 0.0f); // z
-        // bottom left
-        positions.add(-0.5f); // x
-        positions.add(-0.5f); // y
-        positions.add( 0.0f); // z
-        // bottom right
-        positions.add( 0.5f); // x
-        positions.add(-0.5f); // y
-        positions.add( 0.0f); // z
-        // top right
-        positions.add( 0.5f); // x
-        positions.add( 0.5f); // y
-        positions.add( 0.0f); // z
-
-
-        // texture coordinates
-
-        // top left of image
-        textureCoordinates.add(0.0f); // x
-        textureCoordinates.add(0.0f); // y
-        // bottom left of image
-        textureCoordinates.add(0.0f); // x
-        textureCoordinates.add(1.0f); // y
-        // bottom right of image
-        textureCoordinates.add(1.0f); // x
-        textureCoordinates.add(1.0f); // y
-        // top right of image
-        textureCoordinates.add(1.0f); // x
-        textureCoordinates.add(0.0f); // y
-
-        // indices
-
-        // Tri 1
-        indices.add(0);
-        indices.add(1);
-        indices.add(2);
-
-        // Tri 2
-        indices.add(2);
-        indices.add(3);
-        indices.add(0);
+//        positions.add(-0.5f); // x
+//        positions.add( 0.5f); // y
+//        positions.add( 0.0f); // z
+//        // bottom left
+//        positions.add(-0.5f); // x
+//        positions.add(-0.5f); // y
+//        positions.add( 0.0f); // z
+//        // bottom right
+//        positions.add( 0.5f); // x
+//        positions.add(-0.5f); // y
+//        positions.add( 0.0f); // z
+//        // top right
+//        positions.add( 0.5f); // x
+//        positions.add( 0.5f); // y
+//        positions.add( 0.0f); // z
+//
+//
+//        // texture coordinates
+//
+//        // top left of image
+//        textureCoordinates.add(0.0f); // x
+//        textureCoordinates.add(0.0f); // y
+//        // bottom left of image
+//        textureCoordinates.add(0.0f); // x
+//        textureCoordinates.add(1.0f); // y
+//        // bottom right of image
+//        textureCoordinates.add(1.0f); // x
+//        textureCoordinates.add(1.0f); // y
+//        // top right of image
+//        textureCoordinates.add(1.0f); // x
+//        textureCoordinates.add(0.0f); // y
+//
+//        // indices
+//
+//        // Tri 1
+//        indices.add(0);
+//        indices.add(1);
+//        indices.add(2);
+//
+//        // Tri 2
+//        indices.add(2);
+//        indices.add(3);
+//        indices.add(0);
 
         // FIXME: end verbose mess here
 
