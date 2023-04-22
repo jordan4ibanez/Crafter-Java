a8dd5a73b59744b90b19e0179bebd196040f7339
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-21 22:35:01 -0400

    Add complex logic flow

--- src/main/java/org/crafter/engine/world_generation/chunk_mesh_generation/ChunkMeshWorker.java
+++ src/main/java/org/crafter/engine/world_generation/chunk_mesh_generation/ChunkMeshWorker.java
@@ -1,19 +1,20 @@
 package org.crafter.engine.world_generation.chunk_mesh_generation;
 
-import org.crafter.engine.world.block.BlockDefinition;
 import org.crafter.engine.world.block.BlockDefinitionContainer;
 import org.crafter.engine.world.chunk.Chunk;
-import org.joml.Vector3ic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChunkMeshWorker {
 
     private final BlockDefinitionContainer definitionContainer;
+
+
     public ChunkMeshWorker(BlockDefinitionContainer definitionContainer) {
         this.definitionContainer = definitionContainer;
-    }
 
+    }
 
     /**
      * @param chunk A Thread-safe clone passed in from the ChunkMeshGenerator.
@@ -27,33 +28,65 @@ public class ChunkMeshWorker {
         final int WIDTH = chunk.getWidth();
         final int DEPTH = chunk.getDepth();
 
+        /*
+        It works its way:
+        Left to right (0-15 x)
+        -> Front to back (0-15 z)
+        --> Bottom to top (0-15 [differs depending on stack] y)
+         */
+        for (int y = STACK_HEIGHT * stackPosition; y < STACK_HEIGHT * (stackPosition + 1); y++) {
+            for (int z = 0; z < DEPTH; z++) {
+                for (int x = 0; x < WIDTH; x++) {
+
+                    branchPathOfGeneration(chunk, x, y, z);
+                }
+            }
+        }
+    }
+
+    private void branchPathOfGeneration(final Chunk chunk, final int x, final int y, final int z) {
+        // Fixme: This might be a bit too slow
+
         final int[] chunkData = chunk.getData();
 
-        for (int y = STACK_HEIGHT * stackPosition; y < STACK_HEIGHT * (stackPosition + 1); y++) {
-//            System.out.println(y);
-            for (int x = 0; x < WIDTH; x++) {
-                for (int z = 0; z < DEPTH; z++) {
-
-                    // Fixme: This might be a bit too slow
-
-                    final int ID = chunk.getBlockID(chunkData[chunk.positionToIndex(x,y,z)]);
-                    // Testing
+        final int ID = chunk.getBlockID(chunkData[chunk.positionToIndex(x,y,z)]);
+        // Testing
 //                    chunk.printBits(ID);
 //                    String internalName = definitionContainer.getDefinition(ID).getInternalName();
 //                    System.out.println("Block (" + internalName + ") is at: (" + x + ", " + y + ", " + z + ")");
 
-                    // Note: -Z is facing forwards +X is facing right
-                    final int neighborFront = getNeighbor(chunk, x, y, z - 1);
-                    final int neighborBack = getNeighbor(chunk, x, y, z + 1);
-                    final int neighborLeft = getNeighbor(chunk, x - 1, y, z);
-                    final int neighborRight = getNeighbor(chunk, x + 1, y, z);
-                    final int neighborBottom = getNeighbor(chunk, x, y - 1, z);
-                    final int neighborTop = getNeighbor(chunk, x, y + 1, z);
+        // Note: -Z is facing forwards +X is facing right
+        //TODO: implement this
+        final int neighborFront = getNeighbor(chunk, x, y, z - 1);
+        final int neighborBack = getNeighbor(chunk, x, y, z + 1);
+        final int neighborLeft = getNeighbor(chunk, x - 1, y, z);
+        final int neighborRight = getNeighbor(chunk, x + 1, y, z);
+        final int neighborBottom = getNeighbor(chunk, x, y - 1, z);
+        final int neighborTop = getNeighbor(chunk, x, y + 1, z);
 
+        //fixme: for now, just render out each normal block brute force
 
-                }
+        switch (definitionContainer.getDefinition(ID).getDrawType()) {
+            case BLOCK -> {
+                blockDrawType(x, y, z);
             }
+            case GLASS, PLANT, TORCH, LEAVES, BLOCK_BOX, LIQUID_FLOW, LIQUID_SOURCE -> {
+                //todo;
+            }
+            case AIR -> {
+                // do nothing
+                return;
+            }
+            case DEFAULT ->
+                    throw new RuntimeException("ChunkMeshWorker: A block definition has a DEFAULT drawtype!");
+
+            default ->
+                    throw new IllegalStateException("Unexpected value: " + definitionContainer.getDefinition(ID).getDrawType());
         }
+    }
+
+    private void blockDrawType(final int x, final int y, final int z) {
+
     }
 
     private int getNeighbor(Chunk chunk, final int x, final int y, final int z) {
