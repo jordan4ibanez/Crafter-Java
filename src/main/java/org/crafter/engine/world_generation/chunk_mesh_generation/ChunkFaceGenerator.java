fc2e57d98c974dbd8e575592b8140b3e3416ec39
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-21 22:35:04 -0400

    Create ChunkFaceGenerator.java

---
+++ src/main/java/org/crafter/engine/world_generation/chunk_mesh_generation/ChunkFaceGenerator.java
@@ -0,0 +1,15 @@
+package org.crafter.engine.world_generation.chunk_mesh_generation;
+
+import java.util.HashMap;
+
+public class ChunkFaceGenerator {
+    private final HashMap<String, float[]> faces;
+
+    public ChunkFaceGenerator() {
+        faces = new HashMap<>();
+        faces.put("front", new float[]{
+
+        });
+    }
+
+}
