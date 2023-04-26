75caecc85c5ea5f39c40c50956ae7a14ec578c25
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-04-25 21:50:17 -0400

    Add some utility methods

--- src/main/java/org/crafter/engine/camera/Camera.java
+++ src/main/java/org/crafter/engine/camera/Camera.java
@@ -130,6 +130,26 @@ public final class Camera {
     }
 
     public static Vector3ic getHorizontalDirection() {
+        startWork();
 
+        // Work goes here
+
+
+        return getWork();
+    }
+
+
+    /**
+     * This is simply to neaten up the plain english readability of this.
+     * I also don't want to forget to clear out the old data
+     */
+    private static void startWork() {
+        workerVector.zero();
+    }
+    /**
+     * this is because I don't feel like casting the worker vector into 3ic every time
+     */
+    private static Vector3ic getWork() {
+        return (Vector3ic) workerVector;
     }
 }
