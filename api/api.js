98175086c768c05ed4899ab857f31c89e19054f1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-09 18:03:50 -0400

    Add more functionality to api

--- api/api.js
+++ api/api.js
@@ -5,13 +5,26 @@ TODO: Figure out more things like biomes
 TODO:
 */
 
-
-var FileReader = Java.type("org.crafter.engine.utility.FileReader");
-const JavaScriptAPI = Java.type("org.crafter.engine.api.JavaScriptAPI");
+/*
+API is the base of the API which loads up class variables into the crafter array.
+This allows for mods to index DIRECTLY into the engine without needing to be compiled.
+*/
 
 // Lua equivalent!
-var doFile = JavaScriptAPI.runCode;
+var doFile;
+// Lua addition!
+var readFileToString;
+
+// Auto executing localized scope discards
+!function locals(){
+    var FileReader = Java.type("org.crafter.engine.utility.FileReader");
+    var JavaScriptAPI = Java.type("org.crafter.engine.api.JavaScriptAPI");
+    doFile = JavaScriptAPI.runCode;
+    readFileToString = FileReader.getFileString;
+}()
 
 var crafter = [];
 
 doFile("api/testing.js");
+
+var stringy = readFileToString("api/testing.js");
