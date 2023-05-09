a783967c27aabe1baab77df2ab88d1e1143be366
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-09 17:46:12 -0400

    Move this around

--- api/api.js
+++ api/api.js
@@ -1,5 +1,13 @@
+//TODO: load up mods from JavaScript
+/*
+TODO: Use java classes as if they were compiled :D
+TODO: Figure out more things like biomes
+TODO:
+*/
+
+
 var FileReader = Java.type("org.crafter.engine.utility.FileReader");
-var JavaScriptAPI = Java.type("org.crafter.engine.api.JavaScriptAPI");
+const JavaScriptAPI = Java.type("org.crafter.engine.api.JavaScriptAPI");
 
 // Lua equivalent!
 var doFile = JavaScriptAPI.runCode;
@@ -7,10 +15,3 @@ var doFile = JavaScriptAPI.runCode;
 var crafter = [];
 
 doFile("api/testing.js");
-
-//TODO: load up mods from JavaScript
-/*
-TODO: Use java classes as if they were compiled :D
-TODO: Figure out more things like biomes
-TODO:
-*/
