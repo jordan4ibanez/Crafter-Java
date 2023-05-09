76659bb1594bdfdb5b302fbfce9da65bfbb109c4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-09 18:51:07 -0400

    Reflect changes

--- api/api.js
+++ api/api.js
@@ -8,6 +8,7 @@ TODO:
 /*
 API is the base of the API which loads up class variables into the crafter array.
 This allows for mods to index DIRECTLY into the engine without needing to be compiled.
+Limitations: Ecmascript 5 :( No constants, anything can be overridden.
 */
 
 // Lua equivalent!
@@ -15,11 +16,11 @@ var doFile;
 // Lua addition!
 var readFileToString;
 
-// Auto executing localized scope discards
-!function locals(){
+// Auto executing lambda localized variable scope discards
+!function(){
     var FileReader = Java.type("org.crafter.engine.utility.FileReader");
-    var JavaScriptAPI = Java.type("org.crafter.engine.api.JavaScriptAPI");
-    doFile = JavaScriptAPI.runCode;
+    var API = Java.type("org.crafter.engine.api.API");
+    doFile = API.runCode;
     readFileToString = FileReader.getFileString;
 }()
 
@@ -28,3 +29,4 @@ var crafter = [];
 doFile("api/testing.js");
 
 var stringy = readFileToString("api/testing.js");
+print(stringy);
