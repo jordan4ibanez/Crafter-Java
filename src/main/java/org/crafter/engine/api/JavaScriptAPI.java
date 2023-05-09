8b7c492a411ae15193cea49c98e2f924a3564455
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-08 21:25:25 -0400

    Add current JS testing

--- src/main/java/org/crafter/engine/api/JavaScriptAPI.java
+++ src/main/java/org/crafter/engine/api/JavaScriptAPI.java
@@ -1,9 +1,8 @@
 package org.crafter.engine.api;
 
-import javax.script.Bindings;
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import org.openjdk.nashorn.api.scripting.JSObject;
+
+import javax.script.*;
 
 import static org.crafter.engine.utility.FileReader.getFileString;
 
@@ -11,14 +10,24 @@ public final class JavaScriptAPI {
     private static ScriptEngine javaScript;
     private static Bindings bindings;
 
+    private static Compilable compiler;
+    private static Invocable invoker;
+
     private JavaScriptAPI(){}
     public static void initialize() {
         javaScript = new ScriptEngineManager().getEngineByName("Nashorn");
         bindings = javaScript.getBindings(ScriptContext.ENGINE_SCOPE);
+        compiler = (Compilable) javaScript;
+        invoker  = (Invocable) javaScript;
 
         // javaScript.put("test", "hi there");
         runCode("api/api.js");
 
+        Object blah = invoke("getX");
+        System.out.println(blah.getClass());
+        System.out.println(blah);
+
+        runCode("mods/crafter/main.js");
 
     }
 
@@ -35,4 +44,21 @@ public final class JavaScriptAPI {
             throw new RuntimeException("API ERROR!: " + e);
         }
     }
+
+    /**
+     * This is a runtime exception version of the javax invoker method.
+     */
+    public static Object invoke(String functionName, Object... args) {
+
+        try {
+            return invoker.invokeFunction(functionName, args);
+        } catch (Exception e) {
+            throw new RuntimeException("API ERROR!: " + e);
+        }
+//        return test;
+    }
+    /**
+     * Below are the basic typing implementations of the invoke function.
+     */
+
 }
