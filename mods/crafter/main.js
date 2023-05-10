fda0c77488a5a397ff71174de2d7203bed0d41dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-09 22:30:43 -0400

    Register all blocks

--- mods/crafter/main.js
+++ mods/crafter/main.js
@@ -1,5 +1,17 @@
 crafter.registerBlock(
+    new BlockDefinition("crafter:stone")
+        .setReadableName("Stone")
+        .setTextures(["stone.png","stone.png","stone.png","stone.png","stone.png","stone.png"])
+);
+
+crafter.registerBlock(
+    new BlockDefinition("crafter:dirt")
+        .setReadableName("Dirt")
+        .setTextures(["dirt.png","dirt.png","dirt.png","dirt.png","dirt.png","dirt.png"])
+);
+
+crafter.registerBlock(
     new BlockDefinition("crafter:grass")
         .setReadableName("Grass")
-        .setTextures(["goop", "gop", "gleep", "shmeep", "shmoop", "radda"])
+        .setTextures(["grass.png","grass.png","grass.png","grass.png","dirt.png","grass.png"])
 );
--- old_lua_api/old/main.lua
+++ old_lua_api/old/main.lua
@@ -10,14 +10,14 @@ crafter.registerBlock({
     internalName = "crafter:stone";
     readableName = "Stone";
     drawType = crafter.blockDrawTypes.BLOCK;
-    textures = {"stone.png","stone.png","stone.png","stone.png","stone.png","stone.png"};
+    textures = {};
     walkable = true;
 })
 crafter.registerBlock({
     internalName = "crafter:dirt";
     readableName = "Dirt";
     drawType = crafter.blockDrawTypes.BLOCK;
-    textures = {"dirt.png","dirt.png","dirt.png","dirt.png","dirt.png","dirt.png"};
+    textures = {};
     walkable = true;
 })
 crafter.registerBlock({
