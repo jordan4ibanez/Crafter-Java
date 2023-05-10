25a41f09282837c304ddae2a2e527866f6f54edc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-09 21:52:17 -0400

    Implement Grass with specifically wrong textures for next step

--- mods/crafter/main.js
+++ mods/crafter/main.js
@@ -1,3 +1,5 @@
 crafter.registerBlock(
     new BlockDefinition("crafter:grass")
-)
+        .setReadableName("Grass")
+        .setTextures(["goop", "gop", "gleep", "shmeep", "shmoop", "radda"])
+);
