98ad96a8203c8e2f390afaf6db5b45d211a6602f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-05-10 20:42:19 -0400

    Groovy->Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id "com.github.johnrengelman.shadow" version "8.1.1"
-    id "java"
-    id "application"
-    id "base"
-    id "org.jetbrains.kotlin.jvm" version "1.8.21"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("java")
+    id("application")
+    id("base")
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
 }
 
-project.ext.lwjglVersion = "3.3.1"
-project.ext.jomlVersion = "1.10.5"
+val lwjglVersion = "3.3.1"
+val jomlVersion = "1.10.5"
 
-group "org.crafter"
-version "Continuous-Build"
+val group = "org.crafter"
+val version = "Continuous-Build"
 
-final String releaseVersion = "v0.0.2 - Pre-Alpha"
-final String[] natives = ["natives-linux", "natives-macos", "natives-windows"]
+val releaseVersion = "v0.0.2 - Pre-Alpha"
+val natives = arrayOf("natives-linux", "natives-macos", "natives-windows")
 
 repositories {
     mavenCentral()
@@ -21,87 +21,84 @@ repositories {
 
 dependencies {
 
-    testImplementation "org.junit.jupiter:junit-jupiter-api:5.8.1"
-    testRuntimeOnly "org.junit.jupiter:junit-jupiter-engine:5.8.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
-    implementation platform("org.lwjgl:lwjgl-bom:$lwjglVersion")
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
 
-    implementation "org.lwjgl:lwjgl"
-    implementation "org.lwjgl:lwjgl-assimp"
-    implementation "org.lwjgl:lwjgl-glfw"
-    implementation "org.lwjgl:lwjgl-openal"
-    implementation "org.lwjgl:lwjgl-opengl"
-    implementation "org.lwjgl:lwjgl-stb"
+    implementation("org.lwjgl:lwjgl")
+    implementation("org.lwjgl:lwjgl-assimp")
+    implementation("org.lwjgl:lwjgl-glfw")
+    implementation("org.lwjgl:lwjgl-openal")
+    implementation("org.lwjgl:lwjgl-opengl")
+    implementation("org.lwjgl:lwjgl-stb")
 
-    implementation "com.fasterxml.jackson.core:jackson-core:2.14.2"
-    implementation "com.fasterxml.jackson.core:jackson-databind:2.14.2"
-    implementation "com.fasterxml.jackson.core:jackson-annotations:2.14.2"
+    implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.2")
 
-    implementation "party.iroiro.luajava:luajava:3.4.0"
-    implementation "party.iroiro.luajava:luajit:3.4.0"
-    runtimeOnly "party.iroiro.luajava:luajit-platform:3.4.0:natives-desktop"
+    implementation("org.apache.commons:commons-lang3:3.12.0")
 
-    implementation "org.apache.commons:commons-lang3:3.12.0"
-
-    implementation "org.openjdk.nashorn:nashorn-core:15.4"
+    implementation("org.openjdk.nashorn:nashorn-core:15.4")
 
 
-    for (OS in natives) {
-        runtimeOnly "org.lwjgl:lwjgl::$OS"
-        runtimeOnly "org.lwjgl:lwjgl-assimp::$OS"
-        runtimeOnly "org.lwjgl:lwjgl-glfw::$OS"
-        runtimeOnly "org.lwjgl:lwjgl-openal::$OS"
-        runtimeOnly "org.lwjgl:lwjgl-opengl::$OS"
-        runtimeOnly "org.lwjgl:lwjgl-stb::$OS"
+    natives.forEach{
+        runtimeOnly("org.lwjgl:lwjgl::$it")
+        runtimeOnly("org.lwjgl:lwjgl-assimp::$it")
+        runtimeOnly("org.lwjgl:lwjgl-glfw::$it")
+        runtimeOnly("org.lwjgl:lwjgl-openal::$it")
+        runtimeOnly("org.lwjgl:lwjgl-opengl::$it")
+        runtimeOnly("org.lwjgl:lwjgl-stb::$it")
     }
 
-    implementation "org.joml:joml:${jomlVersion}"
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+    implementation("org.joml:joml:${jomlVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
-test {
+
+tasks.test {
     useJUnitPlatform()
 }
 
 application {
-    mainClassName = "org.crafter.Main"
+    mainClass.set("org.crafter.Main")
 }
 
-jar {
+tasks.jar {
     manifest {
-        attributes(
-                "Main-Class": "org.crafter.Main"
-        )
+        // This is a hashmap! I'm writing this comment in case you reading this are learning from this project!
+        attributes["Main-Class"] = "org.crafter.Main"
     }
 }
 
 // This is deprecated, but there"s no alternative I can find
 gradle.taskGraph.afterTask {
-    Task task,TaskState state ->
-        if (task.name == "startShadowScripts") {
-            println("ShadowJar: Compiling and jarifying game!")
-            if (state.failure) {
-                println "$task.name FAILED"
-            } else {
-                println "$task.name SUCCESSFULL"
-            }
-        } else if (task.name == "compileJava") {
-            if (!compileJava.state.upToDate) {
-                for (OS in natives) {
-                    println("CompileJava: Adding $OS natives")
-                }
+    val name = this.name
+
+    if (name == "startShadowScripts") {
+        println("ShadowJar: Compiling and jarifying game!")
+        if (this.state.failure != null) {
+            println("$name FAILED")
+        } else {
+            println("$name SUCCESSFUL")
+        }
+    } else if (name == "compileJava") {
+        if (!tasks.compileJava.isPresent) {
+            natives.forEach {
+                println("CompileJava: Adding $it natives")
             }
         }
+    }
 }
 
-shadowJar {
-    mainClassName = "org.crafter.Main"
+tasks.shadowJar {
+    project.setProperty("mainClassName", "org.crafter.Main")
     archiveBaseName.set("Crafter")
     archiveClassifier.set("")
     archiveVersion.set("")
@@ -113,36 +110,37 @@ buildScan {
 }
 
 // This runs the runShadowScripts then packs up the full game so users can run it easily, but only if fullGame flag given
-final String[] dirs = ["fonts", "shaders", "textures"]
+val dirs = arrayOf("fonts", "shaders", "textures")
 
-tasks.register("packageFullGame", Zip)  {
+tasks.register<Zip>("packageFullGame") {
 
     println("Zipping full build of " + releaseVersion + "!")
 
     // This is just called "crafter" so people can simply drop the folder into the existing one and replace contents
-    final String baseFolder = "crafter"
+    val baseFolder = "crafter"
 
-    archiveFileName = "CrafterLatestRelease.zip"
+//    val archiveFileName = "CrafterLatestRelease.zip"
 
     // All game dirs
     for (directory in dirs) {
         println("Zipping folder: $directory")
         from("$directory/") {
-            include "*"
+            include("*")
             into("$baseFolder/$directory")
         }
     }
     // Now put the jar in
     from("build/libs") {
         println("Zipping: Jar File")
-        include "*"
-        into("$baseFolder")
+        include("*")
+        into(baseFolder)
     }
 
     doLast {
-        for (thisFile in fileTree("build/distributions").filter { it.isFile() }.files.name) {
-            if (thisFile != "CrafterLatestRelease.zip") {
-                project.delete("build/distributions/" + thisFile)
+        for (thisFile in fileTree("build/distributions")
+            .filter { it.isFile }.files) {
+            if (!thisFile.equals("CrafterLatestRelease.zip")) {
+                project.delete("build/distributions/$thisFile")
                 println("Zipping: deleted $thisFile")
             }
         }
@@ -150,5 +148,5 @@ tasks.register("packageFullGame", Zip)  {
     }
 }
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
