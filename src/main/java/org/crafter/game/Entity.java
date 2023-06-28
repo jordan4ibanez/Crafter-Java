bff315dddb2b4df79ba9dba129846f0af336ead0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-06-28 02:20:13 -0400

    Lay down framework for Entity

--- src/main/java/org/crafter/game/Entity.java
+++ src/main/java/org/crafter/game/Entity.java
@@ -17,14 +17,64 @@
  */
 package org.crafter.game;
 
+import org.joml.Vector2f;
+import org.joml.Vector2fc;
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
+
 /**
  * An entity in this game is defined as having:
- *  1. Size
- *  2. Shape (cuboid)
- *  3. Position
- *  4. Velocity
+ *  1. Position
+ *  2. Size/Shape (cuboid)
+ *  3. Velocity
  *  Anything can inherit from this. But SHOULD it, is the main thing to think about before it is done.
+ *  Size is based from it's base position (0,0,0) and scales OUT and UP.
+ *
+ *
  * Todo: Maybe particles can be ghosts or points or something. This might be too much infrastructure for them.
+ *
+ * README:
+ * TODO: Is this an engine component or a game component? Give this some thought.
  */
 public class Entity {
+    private final Vector3f position = new Vector3f();
+
+    // 2D size. Width/Length are identical. So Vector2f(Width/Length, Height) (x,y)
+    private final Vector2f size = new Vector2f();
+    private final Vector3f velocity = new Vector3f();
+
+    // FIXME: Entities might need to super into this. If this happens, remove this blank constructor.
+    public Entity() { // number one :P
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+    public void setPosition(float x, float y, float z) {
+        position.set(x,y,z);
+    }
+    public void setPosition(Vector3fc position) {
+        this.position.set(position);
+    }
+
+
+    public Vector2f getSize() {
+        return size;
+    }
+    public void setSize(float x, float y) {
+        size.set(x,y);
+    }
+    public void setSize(Vector2fc size) {
+        this.size.set(size);
+    }
+
+    public Vector3f getVelocity() {
+        return velocity;
+    }
+    public void setVelocity(float x, float y, float z) {
+        velocity.set(x,y,z);
+    }
+    public void setVelocity(Vector3fc velocity) {
+        this.velocity.set(velocity);
+    }
 }
