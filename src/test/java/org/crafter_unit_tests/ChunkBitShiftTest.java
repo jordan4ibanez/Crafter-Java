d7a62ddbfb45fb1a581b50d53df03cced648aed6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-03 01:49:49 -0400

    Fix the unit tests

--- src/test/java/org/crafter_unit_tests/ChunkBitShiftTest.java
+++ src/test/java/org/crafter_unit_tests/ChunkBitShiftTest.java
@@ -31,39 +31,39 @@ public class ChunkBitShiftTest {
 
     @Test
     public void testChunkBlockID() {
-        Chunk testChunk = new Chunk(0,0);
+
         Random random = new Random((int) (new Date().getTime()/1000));
 
         for (int i = 0; i < testAmount; i++) {
             final int chosenID = random.nextInt(65_535);
-            final int testBlock = testChunk.setBlockID(0, chosenID);
-            final int gottenID = testChunk.getBlockID(testBlock);
+            final int testBlock = Chunk.setBlockID(0, chosenID);
+            final int gottenID = Chunk.getBlockID(testBlock);
             assertEquals(chosenID, gottenID);
         }
     }
 
     @Test
     public void testChunkBlockLight() {
-        Chunk testChunk = new Chunk(0,0);
+
         Random random = new Random((int) (new Date().getTime()/1000));
 
         for (int i = 0; i < testAmount; i++) {
             final int chosenLight = random.nextInt(15);
-            final int testBlock = testChunk.setBlockLight(0, chosenLight);
-            final int gottenLight = testChunk.getBlockLight(testBlock);
+            final int testBlock = Chunk.setBlockLight(0, chosenLight);
+            final int gottenLight = Chunk.getBlockLight(testBlock);
             assertEquals(chosenLight, gottenLight);
         }
     }
 
     @Test
     public void testChunkBlockState() {
-        Chunk testChunk = new Chunk(0,0);
+
         Random random = new Random((int) (new Date().getTime()/1000));
 
         for (int i = 0; i < testAmount; i++) {
             final int chosenState = random.nextInt(16);
-            final int testBlock = testChunk.setBlockState(0, chosenState);
-            final int gottenState = testChunk.getBlockState(testBlock);
+            final int testBlock = Chunk.setBlockState(0, chosenState);
+            final int gottenState = Chunk.getBlockState(testBlock);
             assertEquals(chosenState, gottenState);
         }
     }
@@ -74,7 +74,6 @@ public class ChunkBitShiftTest {
     @Test
     public void testAllBitManipulation() {
 
-        Chunk testChunk = new Chunk(0,0);
         Random random = new Random((int) (new Date().getTime()/1000));
 
         int testBlock = 0;
@@ -85,34 +84,34 @@ public class ChunkBitShiftTest {
             final int chosenLight = random.nextInt(15);
             final int chosenState = random.nextInt(15);
 
-            testBlock = testChunk.setBlockID(testBlock, chosenID);
+            testBlock = Chunk.setBlockID(testBlock, chosenID);
 
-            assertEquals(chosenID, testChunk.getBlockID(testBlock));
+            assertEquals(chosenID, Chunk.getBlockID(testBlock));
 
-            testBlock = testChunk.setBlockLight(testBlock, chosenLight);
+            testBlock = Chunk.setBlockLight(testBlock, chosenLight);
 
-            assertEquals(chosenID, testChunk.getBlockID(testBlock));
-            assertEquals(chosenLight, testChunk.getBlockLight(testBlock));
+            assertEquals(chosenID, Chunk.getBlockID(testBlock));
+            assertEquals(chosenLight, Chunk.getBlockLight(testBlock));
 
-            testBlock = testChunk.setBlockState(testBlock, chosenState);
+            testBlock = Chunk.setBlockState(testBlock, chosenState);
 
-            assertEquals(chosenID, testChunk.getBlockID(testBlock));
-            assertEquals(chosenLight, testChunk.getBlockLight(testBlock));
-            assertEquals(chosenState, testChunk.getBlockState(testBlock));
+            assertEquals(chosenID, Chunk.getBlockID(testBlock));
+            assertEquals(chosenLight, Chunk.getBlockLight(testBlock));
+            assertEquals(chosenState, Chunk.getBlockState(testBlock));
 
             // Now we're going in reverse order to double-check
 
-            testBlock = testChunk.setBlockLight(testBlock, chosenLight);
+            testBlock = Chunk.setBlockLight(testBlock, chosenLight);
 
-            assertEquals(chosenID, testChunk.getBlockID(testBlock));
-            assertEquals(chosenLight, testChunk.getBlockLight(testBlock));
-            assertEquals(chosenState, testChunk.getBlockState(testBlock));
+            assertEquals(chosenID, Chunk.getBlockID(testBlock));
+            assertEquals(chosenLight, Chunk.getBlockLight(testBlock));
+            assertEquals(chosenState, Chunk.getBlockState(testBlock));
 
-            testBlock = testChunk.setBlockID(testBlock, chosenID);
+            testBlock = Chunk.setBlockID(testBlock, chosenID);
 
-            assertEquals(chosenID, testChunk.getBlockID(testBlock));
-            assertEquals(chosenLight, testChunk.getBlockLight(testBlock));
-            assertEquals(chosenState, testChunk.getBlockState(testBlock));
+            assertEquals(chosenID, Chunk.getBlockID(testBlock));
+            assertEquals(chosenLight, Chunk.getBlockLight(testBlock));
+            assertEquals(chosenState, Chunk.getBlockState(testBlock));
         }
 
     }
