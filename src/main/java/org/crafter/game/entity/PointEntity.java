56e1396186ed0809f22dba05f298564260247176
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-03 03:13:13 -0400

    Create PointEntity.java

---
+++ src/main/java/org/crafter/game/entity/PointEntity.java
@@ -0,0 +1,62 @@
+/*
+ Crafter - A blocky game (engine) written in Java with LWJGL.
+ Copyright (C) 2023  jordan4ibanez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.crafter.game.entity;
+
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
+
+/**
+ * A point entity in this game is defined as having:
+ * 1. Position
+ * 2. Velocity
+ * Anything can inherit from this. But SHOULD it, is the main thing to think about before it is done.
+ * Point entities do not have size, they are a single point in the world, hence the name.
+ */
+public class PointEntity {
+
+    private final Vector3f position = new Vector3f();
+
+    private final Vector3f velocity = new Vector3f();
+
+    public PointEntity() {
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+    public void setPosition(float x, float y, float z) {
+        position.set(x,y,z);
+    }
+    public void setPosition(Vector3fc position) {
+        this.position.set(position);
+    }
+
+    public Vector3f getVelocity() {
+        return velocity;
+    }
+    public void setVelocity(float x, float y, float z) {
+        velocity.set(x,y,z);
+    }
+    public void setVelocity(Vector3fc velocity) {
+        this.velocity.set(velocity);
+    }
+
+    public void worldCollisionDetection() {
+        // Todo
+    }
+}
