17f8c0cb55d80e17d6b19adeb74de8f2d0ac1f73
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-03 03:13:23 -0400

    Entity extends PointEntity

--- src/main/java/org/crafter/game/entity/Entity.java
+++ src/main/java/org/crafter/game/entity/Entity.java
@@ -19,8 +19,6 @@ package org.crafter.game.entity;
 
 import org.joml.Vector2f;
 import org.joml.Vector2fc;
-import org.joml.Vector3f;
-import org.joml.Vector3fc;
 
 /**
  * An entity in this game is defined as having:
@@ -30,33 +28,15 @@ import org.joml.Vector3fc;
  *  Anything can inherit from this. But SHOULD it, is the main thing to think about before it is done.
  *  Size is based from it's base position (0,0,0) and scales OUT and UP.
  *
- *
- * Todo: Maybe particles can be ghosts or points or something. This might be too much infrastructure for them.
- *
- * README:
- * TODO: Is this an engine component or a game component? Give this some thought.
+ *  Entity extends PointEntity because it is simply a point entity with size.
  */
-public class Entity {
-    private final Vector3f position = new Vector3f();
+public class Entity extends PointEntity{
 
     // 2D size. Width/Length are identical. So Vector2f(Width/Length, Height) (x,y)
     private final Vector2f size = new Vector2f();
-    private final Vector3f velocity = new Vector3f();
 
-    // FIXME: Entities might need to super into this. If this happens, remove this blank constructor.
     public Entity() { // number one :P
     }
-
-    public Vector3f getPosition() {
-        return position;
-    }
-    public void setPosition(float x, float y, float z) {
-        position.set(x,y,z);
-    }
-    public void setPosition(Vector3fc position) {
-        this.position.set(position);
-    }
-
 
     public Vector2f getSize() {
         return size;
@@ -68,13 +48,8 @@ public class Entity {
         this.size.set(size);
     }
 
-    public Vector3f getVelocity() {
-        return velocity;
-    }
-    public void setVelocity(float x, float y, float z) {
-        velocity.set(x,y,z);
-    }
-    public void setVelocity(Vector3fc velocity) {
-        this.velocity.set(velocity);
+    @Override
+    public void worldCollisionDetection() {
+        System.out.println("I am a potato");
     }
 }
