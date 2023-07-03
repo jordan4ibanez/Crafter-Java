9f0eee67c386241568d88af1d2c8cd6ee46422b4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-02 22:17:21 -0400

    Create MobContainer.java

---
+++ src/main/java/org/crafter/game/entity/mob/MobContainer.java
@@ -0,0 +1,30 @@
+/*
+ Crafter - A blocky game (engine) written in Java with LWJGL.
+ Copyright (C) 2023  jordan4ibanez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.crafter.game.entity.mob;
+
+import java.util.HashMap;
+
+public final class MobContainer {
+    HashMap<String, Mob> container = new HashMap<>();
+    private MobContainer() {}
+
+    public void registerMob(Mob mob) {
+        mob.lockOut();
+        container.put(mob.getName(), mob);
+    }
+}
