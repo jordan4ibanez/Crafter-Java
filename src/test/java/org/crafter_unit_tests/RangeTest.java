202cc16f396374e590da0cfe005a494577b0b8b5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-04 00:50:22 -0400

    Create RangeTest.java

---
+++ src/test/java/org/crafter_unit_tests/RangeTest.java
@@ -0,0 +1,38 @@
+/*
+ Crafter - A blocky game (engine) written in Java with LWJGL.
+ Copyright (C) 2023  jordan4ibanez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.crafter_unit_tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.crafter.engine.utility.Range.range;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RangeTest {
+
+    @Test
+    public void increment() {
+        int tracker = 0;
+
+        final int[] test1 = new int[]{0,1,2,3,4,5,6,7,8,9};
+
+        for (int i : range(10)) {
+            assertEquals(i, test1[tracker]);
+            tracker++;
+        }
+    }
+}
