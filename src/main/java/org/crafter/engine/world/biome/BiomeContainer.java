84240da055355d0c5002c44676ff37770e26a080
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-05 02:28:55 -0400

    Create BiomeContainer.java

---
+++ src/main/java/org/crafter/engine/world/biome/BiomeContainer.java
@@ -0,0 +1,35 @@
+/*
+ Crafter - A blocky game (engine) written in Java with LWJGL.
+ Copyright (C) 2023  jordan4ibanez
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.crafter.engine.world.biome;
+
+import java.util.HashMap;
+
+public final class BiomeContainer {
+
+    private static final HashMap<String, Biome> container = new HashMap<>();
+    private BiomeContainer(){}
+
+
+    public static void registerBiome(String name, Biome definition) {
+        container.put(name, definition);
+    }
+
+    public static Biome getBiome(String name) {
+        return container.get(name);
+    }
+}
