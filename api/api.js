ccce38db2e4b27f0474c268129c673a76f9ae8c1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-09 01:04:48 -0400

    Update some comments about limitations

--- api/api.js
+++ api/api.js
@@ -28,7 +28,8 @@ TODO! VERY IMPORTANT! Add comments! See if there is a way to link the java docum
 /*
 API is the base of the API which loads up class variables into the crafter array.
 This allows for mods to index DIRECTLY into the engine without needing to be compiled.
-Limitations: Ecmascript 5 :( No constants, anything can be overridden.
+Limitations: Ecmascript 6. Class architecture is not implemented into nashorn yet.
+Possible implementations: Typescript (one day)
 */
 
 // Lua equivalents!
@@ -45,9 +46,11 @@ const crafter = [];
 
 // Auto executing lambda localized variable scope discards
 !function(){
+
     // Classes from the engine which will disappear after this scope.
     var FileReader = Java.type("org.crafter.engine.utility.FileReader");
     var API = Java.type("org.crafter.engine.api.API");
+
     var BlockDefinitionContainer = Java.type("org.crafter.engine.world.block.BlockDefinitionContainer");
     var BiomeDefinitionContainer = Java.type("org.crafter.engine.world.biome.BiomeDefinitionContainer");
 
