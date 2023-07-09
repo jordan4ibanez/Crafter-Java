25b88386af70047ece8bd39e7b61e809390f1c5d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-09 02:01:44 -0400

    Add a space for literally no reason

--- mods/crafter/biomes.js
+++ mods/crafter/biomes.js
@@ -22,6 +22,7 @@ crafter.registerBiome(
     new BiomeDefinition("classic")
         .setFrequency(0.5)
         .setOctaves(3)
+
         .setGrassLayer("crafter:grass")
         .setDirtLayer("crafter:dirt")
         .setStoneLayer("crafter:stone")
