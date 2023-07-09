003cfd1cce1353ad2c5cc814508a4ecfa66fbb9e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-09 01:52:34 -0400

    Create blocks.js

---
+++ mods/crafter/blocks.js
@@ -0,0 +1,19 @@
+// Block registration
+
+crafter.registerBlock(
+    new BlockDefinition("crafter:stone")
+        .setReadableName("Stone")
+        .setTextures(["stone.png","stone.png","stone.png","stone.png","stone.png","stone.png"])
+);
+
+crafter.registerBlock(
+    new BlockDefinition("crafter:dirt")
+        .setReadableName("Dirt")
+        .setTextures(["dirt.png","dirt.png","dirt.png","dirt.png","dirt.png","dirt.png"])
+);
+
+crafter.registerBlock(
+    new BlockDefinition("crafter:grass")
+        .setReadableName("Grass")
+        .setTextures(["grass.png","grass.png","grass.png","grass.png","dirt.png","grass.png"])
+);
