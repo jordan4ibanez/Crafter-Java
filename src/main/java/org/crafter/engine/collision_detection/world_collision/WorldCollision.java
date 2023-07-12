003a28a930136c9c95927137d05e3203eb3ea1e9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-12 00:21:44 -0400

    Into the world_collision package

--- src/main/java/org/crafter/engine/collision_detection/world_collision/WorldCollision.java
+++ src/main/java/org/crafter/engine/collision_detection/world_collision/WorldCollision.java
@@ -15,9 +15,7 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package org.crafter.engine.collision_detection;
+package org.crafter.engine.collision_detection.world_collision;
 
-public final class AABBCollision {
-
-    private AABBCollision(){}
+public class WorldCollision {
 }
