c5cebc399f159f71f737e30e9f46d23d85fb52a0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-12 00:47:20 -0400

    Dump this mess into the commit logs

--- src/main/java/org/crafter/engine/collision_detection/world_collision/AABBCollision.java
+++ src/main/java/org/crafter/engine/collision_detection/world_collision/AABBCollision.java
@@ -18,6 +18,7 @@
 package org.crafter.engine.collision_detection.world_collision;
 
 import org.joml.Vector2fc;
+import org.joml.Vector3f;
 import org.joml.Vector3fc;
 
 /**
@@ -26,10 +27,40 @@ import org.joml.Vector3fc;
  */
 final class AABBCollision {
 
+    // This is going to be a bit complex, unfortunately. All this for the sake of optimization.
+    private static final Vector3f min1 = new Vector3f();
+    private static final Vector3f max1 = new Vector3f();
+    private static final Vector3f min1Old = new Vector3f();
+    private static final Vector3f max1Old = new Vector3f();
+    private static final Vector3f min2 = new Vector3f();
+    private static final Vector3f max2 = new Vector3f();
+    private static final Vector3f min2Old = new Vector3f();
+    private static final Vector3f max2Old = new Vector3f();
+
     private AABBCollision(){}
 
+    /**
+     * This needs to be run AFTER the entity moves! You must keep track of the old position and insert it into this!
+     * @param oldPosition1 Entity 1's old position.
+     * @param position1 Entity 1's current position.
+     * @param size1 Entity 1's size.
+     * @param oldPosition2 Entity 2's old position.
+     * @param position2 Entity 2's current position.
+     * @param size2 Entity 2's size.
+     * @return True or false. True if the entity intersects.
+     */
     public static boolean collide(Vector3fc oldPosition1, Vector3fc position1, Vector2fc size1, Vector3fc oldPosition2, Vector3fc position2, Vector2fc size2) {
-        BoundingBox thisBox = thisEntity.getBoundingBox();
+
+        // I said this was gonna be complicated, didn't I?
+        min1Old.set(oldPosition1.x() - size1.x(), oldPosition1.y(), oldPosition1.z() - size1.x());
+        max1Old.set(oldPosition1.x() + size1.x(), oldPosition1.y() + size1.y(), oldPosition1.z() + size1.x());
+        min1.set(position1.x() - size1.x(), position1.y(), position1.z() - size1.x());
+        max1.set(position1.x() + size1.x(), position1.y() + size1.y(), position1.z() + size1.x());
+
+        min2Old.set(oldPosition2.x() - size2.x(), oldPosition2.y(), oldPosition2.z() - size2.x());
+        max2Old.set(oldPosition2.x() + size2.x(), oldPosition2.y() + size2.y(), oldPosition2.z() + size2.x());
+        min2.set(position2.x() - size2.x(), position2.y(), position2.z() - size2.x());
+        max2.set(position2.x() + size2.x(), position2.y() + size2.y(), position2.z() + size2.x());
 
         foreach(otherEntity; thisQuadrant.entitiesWithin.filter !(o = > o != thisEntity)){
 
@@ -38,20 +69,20 @@ final class AABBCollision {
             if (CheckCollisionBoxes(thisBox, otherBox)) {
 
                 // These are 1D collision detections
-                bool bottomWasNotIn = oldBox.min.y > otherBox.max.y;
-                bool bottomIsNowIn = thisBox.min.y <= otherBox.max.y && thisBox.min.y >= otherBox.min.y;
-                bool topWasNotIn = oldBox.max.y < otherBox.min.y;
-                bool topIsNowIn = thisBox.max.y <= otherBox.max.y && thisBox.max.y >= otherBox.min.y;
+                boolean bottomWasNotIn = oldBox.min.y > otherBox.max.y;
+                boolean bottomIsNowIn = thisBox.min.y <= otherBox.max.y && thisBox.min.y >= otherBox.min.y;
+                boolean topWasNotIn = oldBox.max.y < otherBox.min.y;
+                boolean topIsNowIn = thisBox.max.y <= otherBox.max.y && thisBox.max.y >= otherBox.min.y;
 
-                bool leftWasNotIn = oldBox.min.x > otherBox.max.x;
-                bool leftIsNowIn = thisBox.min.x <= otherBox.max.x && thisBox.min.x >= otherBox.min.x;
-                bool rightWasNotIn = oldBox.max.x < otherBox.min.x;
-                bool rightIsNowIn = thisBox.max.x <= otherBox.max.x && thisBox.max.x >= otherBox.min.x;
+                boolean leftWasNotIn = oldBox.min.x > otherBox.max.x;
+                boolean leftIsNowIn = thisBox.min.x <= otherBox.max.x && thisBox.min.x >= otherBox.min.x;
+                boolean rightWasNotIn = oldBox.max.x < otherBox.min.x;
+                boolean rightIsNowIn = thisBox.max.x <= otherBox.max.x && thisBox.max.x >= otherBox.min.x;
 
-                bool backWasNotIn = oldBox.min.z > otherBox.max.z;
-                bool backIsNowIn = thisBox.min.z <= otherBox.max.z && thisBox.min.z >= otherBox.min.z;
-                bool frontWasNotIn = oldBox.max.z < otherBox.min.z;
-                bool frontIsNowIn = thisBox.max.z <= otherBox.max.z && thisBox.max.z >= otherBox.min.z;
+                boolean backWasNotIn = oldBox.min.z > otherBox.max.z;
+                boolean backIsNowIn = thisBox.min.z <= otherBox.max.z && thisBox.min.z >= otherBox.min.z;
+                boolean frontWasNotIn = oldBox.max.z < otherBox.min.z;
+                boolean frontIsNowIn = thisBox.max.z <= otherBox.max.z && thisBox.max.z >= otherBox.min.z;
 
 
                 /// y check first
