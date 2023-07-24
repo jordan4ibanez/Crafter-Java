c57880fd2722652cf4c2555f913e0ad861c04ddb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-24 02:51:27 -0400

    Absolutely overhaul the BM unit tests

--- src/test/java/org/crafter_unit_tests/BlockManipulatorTest.java
+++ src/test/java/org/crafter_unit_tests/BlockManipulatorTest.java
@@ -27,66 +27,109 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class BlockManipulatorTest {
 
     @Test
-    public void testInvalidPositions() {
-
-        // Identical positions
+    public void testInvalidCubicSize() {
+        // Identical positions at 0,0,0
         try {
             setBlockManipulatorPositions(new Vector3i(0,0,0), new Vector3i(0,0,0));
-            fail("Block Manipulator allowed invalid state 0");
+            fail("Block Manipulator allowed invalid cubic size 1");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set the Block Manipulator to 1 block cubic area! Use the single block getters/setters instead!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 0");
+//            System.out.println("Block Manipulator passed cubic size test 1");
+        }
+
+        // Identical positions at -X,0,0
+        try {
+            setBlockManipulatorPositions(new Vector3i(-64,0,0), new Vector3i(-64,0,0));
+            fail("Block Manipulator allowed invalid cubic size 2");
+        } catch (Exception e) {
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set the Block Manipulator to 1 block cubic area! Use the single block getters/setters instead!", e.toString());
+            // This passes
+//            System.out.println("Block Manipulator passed cubic size test 2");
+        }
+
+        // Identical positions at X,0,0
+        try {
+            setBlockManipulatorPositions(new Vector3i(64,0,0), new Vector3i(64,0,0));
+            fail("Block Manipulator allowed invalid cubic size 3");
+        } catch (Exception e) {
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set the Block Manipulator to 1 block cubic area! Use the single block getters/setters instead!", e.toString());
+            // This passes
+//            System.out.println("Block Manipulator passed cubic size test 3");
+        }
+
+        // Identical positions at 0,0,-Z
+        try {
+            setBlockManipulatorPositions(new Vector3i(0,0,-64), new Vector3i(0,0,-64));
+            fail("Block Manipulator allowed invalid cubic size 4");
+        } catch (Exception e) {
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set the Block Manipulator to 1 block cubic area! Use the single block getters/setters instead!", e.toString());
+            // This passes
+//            System.out.println("Block Manipulator passed cubic size test 4");
+        }
+
+        // Identical positions at 0,0,Z
+        try {
+            setBlockManipulatorPositions(new Vector3i(0,0,64), new Vector3i(0,0,64));
+            fail("Block Manipulator allowed invalid cubic size 5");
+        } catch (Exception e) {
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set the Block Manipulator to 1 block cubic area! Use the single block getters/setters instead!", e.toString());
+            // This passes
+//            System.out.println("Block Manipulator passed cubic size test 5");
+        }
+    }
+
+    @Test
+    public void testInvalidPositions() {
+
+        // X
+        try {
+            setBlockManipulatorPositions(new Vector3i(-32,0,0), new Vector3i(-34,1,1));
+            fail("Block Manipulator allowed invalid positions 1");
+        } catch (Exception e) {
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!", e.toString());
+            // This passes
+//            System.out.println("Block Manipulator passed position test 1");
         }
 
         // X
         try {
-            setBlockManipulatorPositions(new Vector3i(1,0,0), new Vector3i(1,1,1));
-            fail("Block Manipulator allowed invalid state 1");
+            setBlockManipulatorPositions(new Vector3i(34,0,0), new Vector3i(32,1,1));
+            fail("Block Manipulator allowed invalid positions 2");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 1");
-        }
-
-        // X
-        try {
-            setBlockManipulatorPositions(new Vector3i(1,2,-1), new Vector3i(1,1,1));
-            fail("Block Manipulator allowed invalid state 2");
-        } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!");
-            // This passes
-            System.out.println("Block Manipulator passed test 2");
+//            System.out.println("Block Manipulator passed position test 2");
         }
 
         // Z
         try {
-            setBlockManipulatorPositions(new Vector3i(-1,0,2), new Vector3i(1,1,1));
-            fail("Block Manipulator allowed invalid state 3");
+            setBlockManipulatorPositions(new Vector3i(0,0,-32), new Vector3i(1,1,-34));
+            fail("Block Manipulator allowed invalid positions 3");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (Z axis) min is greater than or equal to max!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (Z axis) min is greater than or equal to max!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 3");
+//            System.out.println("Block Manipulator passed position test 3");
         }
 
         // Z
         try {
-            setBlockManipulatorPositions(new Vector3i(-1,-1,1), new Vector3i(1,1,0));
-            fail("Block Manipulator allowed invalid state 4");
+            setBlockManipulatorPositions(new Vector3i(0,0,34), new Vector3i(1,1,32));
+            fail("Block Manipulator allowed invalid positions 4");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (Z axis) min is greater than or equal to max!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (Z axis) min is greater than or equal to max!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 4");
+//            System.out.println("Block Manipulator passed position test 4");
         }
 
         // Inverted positions
         try {
-            setBlockManipulatorPositions(new Vector3i(1,1,1), new Vector3i(-1,-1,-1));
-            fail("Block Manipulator allowed invalid state 5");
+            setBlockManipulatorPositions(new Vector3i(1,1,1), new Vector3i(-1,0,-1));
+            fail("Block Manipulator allowed invalid positions 5");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator to invalid min/max! (X axis) min is greater than or equal to max!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 5");
+//            System.out.println("Block Manipulator passed position test 5");
         }
     }
 
@@ -98,9 +141,9 @@ public class BlockManipulatorTest {
             setBlockManipulatorPositions(-64,0,-1, 0,127,0);
             fail("Block Manipulator allowed invalid size 1");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (X axis) limit is 64!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (X axis) limit is 64!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 6");
+//            System.out.println("Block Manipulator passed test 6");
         }
 
         // Z
@@ -108,9 +151,9 @@ public class BlockManipulatorTest {
             setBlockManipulatorPositions(-1,0,-64, 0,127,0);
             fail("Block Manipulator allowed invalid size 2");
         } catch (Exception e) {
-            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (Z axis) limit is 64!");
+            assertEquals("java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (Z axis) limit is 64!", e.toString());
             // This passes
-            System.out.println("Block Manipulator passed test 7");
+//            System.out.println("Block Manipulator passed test 7");
         }
 
         // X
@@ -120,7 +163,7 @@ public class BlockManipulatorTest {
         } catch (Exception e) {
             assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (X axis) limit is 64!");
             // This passes
-            System.out.println("Block Manipulator passed test 8");
+//            System.out.println("Block Manipulator passed test 8");
         }
 
         // Z
@@ -130,7 +173,7 @@ public class BlockManipulatorTest {
         } catch (Exception e) {
             assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (Z axis) limit is 64!");
             // This passes
-            System.out.println("Block Manipulator passed test 9");
+//            System.out.println("Block Manipulator passed test 9");
         }
 
         // X
@@ -140,7 +183,7 @@ public class BlockManipulatorTest {
         } catch (Exception e) {
             assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (X axis) limit is 64!");
             // This passes
-            System.out.println("Block Manipulator passed test 10");
+//            System.out.println("Block Manipulator passed test 10");
         }
 
         // Z
@@ -150,7 +193,20 @@ public class BlockManipulatorTest {
         } catch (Exception e) {
             assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (Z axis) limit is 64!");
             // This passes
-            System.out.println("Block Manipulator passed test 11");
+//            System.out.println("Block Manipulator passed test 11");
         }
+    }
+
+    @Test
+    public void testInvalidYPositions() {
+        // Y min ABOVE Y max
+//        try {
+//            setBlockManipulatorPositions(0,127,0, 1,0,1);
+//            fail("Block Manipulator allowed invalid size 6");
+//        } catch (Exception e) {
+//            assertEquals(e.toString(), "java.lang.RuntimeException: ChunkStorage: Attempted to set Block Manipulator past size limit! (Z axis) limit is 64!");
+//            // This passes
+//            System.out.println("Block Manipulator passed test 11");
+//        }
     }
 }
