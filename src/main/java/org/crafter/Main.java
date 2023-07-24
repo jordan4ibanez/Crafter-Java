6cd4628721416ceae781a85c77b6ab5a96262029
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-24 07:13:19 -0400

    Add some very basic player movement

--- src/main/java/org/crafter/Main.java
+++ src/main/java/org/crafter/Main.java
@@ -21,6 +21,7 @@ import org.crafter.engine.api.API;
 import org.crafter.engine.camera.Camera;
 import org.crafter.engine.controls.Keyboard;
 import org.crafter.engine.controls.Mouse;
+import org.crafter.engine.delta.Delta;
 import org.crafter.engine.gui.font.Font;
 import org.crafter.engine.mesh.MeshStorage;
 import org.crafter.engine.shader.ShaderStorage;
@@ -31,12 +32,16 @@ import org.crafter.engine.world_generation.ChunkThreadDirector;
 import org.crafter.engine.world_generation.chunk_generation.ChunkGenerator;
 import org.crafter.engine.world_generation.chunk_mesh_generation.ChunkMeshGenerator;
 import org.crafter.game.entity.player.Player;
+import org.crafter.game.entity.player.PlayerStorage;
 import org.joml.Vector2i;
+import org.joml.Vector3f;
 
 import static org.crafter.engine.collision_detection.world_collision.Physics.entityPhysics;
+import static org.crafter.engine.utility.GameMath.getHorizontalDirection;
+import static org.crafter.engine.utility.GameMath.yawToLeft;
+import static org.crafter.engine.utility.JOMLUtils.printVec;
 import static org.crafter.game.entity.player.PlayerStorage.*;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_F1;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_F11;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Main {
 
@@ -130,13 +135,14 @@ public class Main {
         Window.clearAll();
 
         ShaderStorage.start("3d");
-
-
         ChunkThreadDirector.runLogic();
 
 
         // Render all players
         if (playerExists("singleplayer")) {
+
+            prototypePlayerMovement();
+
             Player player = getPlayer("singleplayer");
 
             entityPhysics(player);
@@ -160,6 +166,58 @@ public class Main {
 
 
         Window.swapBuffers();
+    }
+
+    /**
+     * PROTOTYPING ONLY movement for the client player.
+     * TODO: MOVE THIS SOMEWHERE THAT MAKES MORE SENSE!
+     */
+    private static void prototypePlayerMovement() {
+
+        // Movement
+        final Vector3f inputMovement = new Vector3f();
+
+        if (Keyboard.keyDown(GLFW_KEY_W)) {
+            inputMovement.z += -1;
+        }
+        if (Keyboard.keyDown(GLFW_KEY_S)) {
+            inputMovement.z += 1;
+        }
+
+        if (Keyboard.keyDown(GLFW_KEY_A)) {
+            inputMovement.x += -1;
+        }
+        if (Keyboard.keyDown(GLFW_KEY_D)) {
+            inputMovement.x += 1;
+        }
+
+//        if (Keyboard.keyDown(GLFW_KEY_SPACE)) {
+//            inputMovement.y += 1;
+//        }
+//        if (Keyboard.keyDown(GLFW_KEY_LEFT_SHIFT) || Keyboard.keyDown(GLFW_KEY_RIGHT_SHIFT)) {
+//            inputMovement.y -= 1;
+//        }
+
+
+        final float yaw = Camera.getYaw();
+        final float movementDelta = Delta.getDelta() * 50;
+
+        // Layered
+        final Vector3f cameraMovementX = new Vector3f();
+        final Vector3f cameraMovementY = new Vector3f();
+        final Vector3f cameraMovementZ = new Vector3f();
+        final Vector3f finalCameraMovement = new Vector3f();
+
+        cameraMovementX.set(getHorizontalDirection(yawToLeft(yaw))).mul(inputMovement.x());
+        cameraMovementY.set(0,inputMovement.y(), 0);
+        cameraMovementZ.set(getHorizontalDirection(yaw)).mul(inputMovement.z());
+
+        // Layer in, and then make it usable with delta
+        finalCameraMovement.set(cameraMovementX.add(cameraMovementY).add(cameraMovementZ)).mul(movementDelta);
+
+        final Player clientPlayer = PlayerStorage.getClientPlayer();
+        finalCameraMovement.y = clientPlayer.getVelocity().y();
+        clientPlayer.setVelocity(finalCameraMovement);
     }
 
     /**
