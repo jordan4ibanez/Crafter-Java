82646414b2bf8d66d6680a30c74ae162754d664f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-27 04:42:10 -0400

    Add some basic testing for actions

---
+++ mods/crafter/scriptingTest.js
@@ -0,0 +1,30 @@
+// Testing out the actions API :D
+crafter.registerOnJoin((player) => {
+    print("Hello, " + player.getName() + "! Welcome to Crafter Classic!");
+});
+
+// You can localize variables like so
+{
+    let x = 0;
+    crafter.registerOnTick((delta) => {
+        x += delta;
+        if (x > 1) {
+            print("tick");
+            x = 0;
+        }
+    });
+}
+
+// This crashes, because x is not defined
+// print("x is: " + x)
+
+// This one runs only once
+crafter.registerOnTimer(
+    2,
+    () => {
+        print("timer api test!");
+    },
+    false
+)
+
+// This one runs forever(
