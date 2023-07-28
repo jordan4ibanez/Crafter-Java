faf6a2c1d0a77410db09fbc4928c5f523725bd77
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-07-28 04:58:55 -0400

    Add new test

--- mods/crafter/scriptingTest.js
+++ mods/crafter/scriptingTest.js
@@ -1,37 +1,56 @@
 // Testing out the actions API :D
-crafter.registerOnJoin((player) => {
-    print("Hello, " + player.getName() + "! Welcome to Crafter Classic!");
-});
+//crafter.registerOnJoin((player) => {
+//    print("Hello, " + player.getName() + "! Welcome to Crafter Classic!");
+//});
 
 // You can localize variables like so
-{
-    let x = 0;
-    crafter.registerOnTick((delta) => {
-        x += delta;
-        if (x > 1) {
-            print("tick");
-            x = 0;
-        }
-    });
-};
+//{
+//    let x = 0;
+//    crafter.registerOnTick((delta) => {
+//        x += delta;
+//        if (x > 1) {
+//            print("tick");
+//            x = 0;
+//        }
+//    });
+//};
 
 // This crashes, because x is not defined
 // print("x is: " + x)
 
 // This one runs only once
-crafter.registerOnTimer(
-    2,
-    false,
-    () => {
-        print("timer api test! (run once)");
-    }
-);
+//crafter.registerOnTimer(
+//    2,
+//    false,
+//    () => {
+//        print("timer api test! (run once)");
+//    }
+//);
 
 // This one runs forever(
+//crafter.registerOnTimer(
+//    3,
+//    true,
+//    (delta) => {
+//        print("Timer api test! (running forever)");
+//    }
+//);
+
+// I call this one "brick walker"
 crafter.registerOnTimer(
-    3,
+    0.5,
     true,
-    (delta) => {
-        print("Timer api test! (running forever)");
+    () => {
+        // 2 ways to iterate
+
+        // Functional
+        crafter.getConnectedPlayers().forEach((player) => {
+            print(player.getName());
+        })
+
+        // Nashorn OOP style
+//        for each (player in crafter.getConnectedPlayers()) {
+//            print(player);
+//        }
     }
-);
+)
